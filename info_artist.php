b3c19d9be912c5f6f6a478fc09ca23d2fa97fd2b
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-01-15 14:24:07 +0100

    layout fixes to margins modal-body row m-0 p-0

--- info_artist.php
+++ info_artist.php
@@ -7,7 +7,7 @@ include "header.php";
 //-----------------------------------------------------------------------------
 
 ?>
-<div class="row">
+<div class="modal-body row m-0 p-0">
   <div class="col-lg-8 order-md-1 order-lg-2 order-xl-2">
     <?php
 
