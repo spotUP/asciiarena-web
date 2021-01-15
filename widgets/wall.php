2507289751dc92f8ad13ace32217b9e6938a7b4d
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-01-15 13:10:16 +0100

    fixed wall width

--- widgets/wall.php
+++ widgets/wall.php
@@ -4,7 +4,7 @@
 	$form = "form_{$wall_id}";
 	$tag = "tag_{$wall_id}";
 ?>
-<div class="container" style="padding-bottom: 16px;">
+<div class="container-fluid" style="padding-bottom: 16px;">
 	<div class="row" id="<?=$wall?>"></div>
 	<?php if (is_logged_in()): ?>
 		<div class="row">
