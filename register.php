ef444c3ac998deb2b0ecede0f3cbc00ee824d1aa
Nicolas Mendoza <mendoza@pvv.ntnu.no>
2021-02-24 14:28:28 +0100

    Avoid error with reserved word "rank"

--- register.php
+++ register.php
@@ -129,7 +129,7 @@ $now = time();
 						exit;
 					}
 					$ask = $_db->prepare("INSERT INTO users
-       (nick,       crew,         password, pwhash, lastactive, current, avatar,               mail,  uploaded, rank,        upload_signature,                              list_view_mode, display_mail,display_messenger)
+       (nick,       crew,         password, pwhash, lastactive, current, avatar,               mail,  uploaded, `rank`,        upload_signature,                              list_view_mode, display_mail,display_messenger)
 VALUES (:check_nick,'Independent','SECRET',:pwhash, :now,       '',      'AvatarDefault.jpg', :mail,   0,        'Inactive', '- -- - aSCIIaRENa - ---- - aSCIIaRENa - -- -','Standard',     'No',        'No'            )
 ");
 					$ask->execute([
