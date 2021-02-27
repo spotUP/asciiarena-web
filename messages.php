be236f5aba1d1214571a4dfd640aaf047a20cc06
Nicolas Mendoza <mendoza@pvv.ntnu.no>
2021-02-27 02:44:29 +0100

    Fetch postername from session

--- messages.php
+++ messages.php
@@ -136,6 +136,7 @@ VALUES (:thread,:posttomember,:postername,:now,:postsubject,:postmessage,1,1)
 
 				exit;
 			}
+			$postername = $_user['nick'];
 			$postsubject = cleanInsert($postsubject);
 			$postmessage = cleanInsertPost($postmessage);
 			$now = time();
@@ -143,7 +144,7 @@ VALUES (:thread,:posttomember,:postername,:now,:postsubject,:postmessage,1,1)
 			doQuery($ask, [
 			  'thread'       => $thread,
 			  'posttomember' => $posttomember,
-			  'nick'         => $nick,
+			  'nick'         => $postername,
 			  'now'          => $now,
 			  'postsubject'  => $postsubject,
 			  'postmessage'  => $postmessage
