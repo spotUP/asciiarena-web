dcfedd292da5e138fb781b3644d5ad6563502a31
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-02-27 22:42:29 +0100

    added myself to debug output

--- tools/debug.php
+++ tools/debug.php
@@ -1,6 +1,6 @@
 <?php
 
-define("DEBUG", ((php_sapi_name() == 'cli-server') || in_array($_SERVER[ "REMOTE_ADDR" ], ["127.0.0.1", "77.53.224.247", "::1"])));
+define("DEBUG", ((php_sapi_name() == 'cli-server') || in_array($_SERVER[ "REMOTE_ADDR" ], ["127.0.0.1", "77.53.224.247", "192.168.0.18", "::1"])));
 
 if (DEBUG) {
   ini_set("display_errors", 1);
