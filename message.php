0e4c3f7a3cd86831b5ea353df75df23a65faed37
Nicolas Mendoza <mendoza@pvv.ntnu.no>
2021-02-28 13:56:56 +0100

    Re-apply changes from 13aaeb38c422e1c7ff5f37706de3a682cceb8e9b

--- message.php
+++ message.php
@@ -165,9 +165,10 @@ if (is_logged_in()) {
 
 			if (isset($_POST[ 'deletemessage' ])) {
 				$deleteid = ($_POST[ 'thread' ]);
+				$deletemsgid = ($_POST[ 'messid' ]);
 
-				$ask = $_db->prepare("delete from messages where thread=:deleteid and postedto=:nick");
-				$ask->execute(['deleteid' => $deleteid, 'nick' => $nick]);
+				$ask = $_db->prepare("delete from messages where thread=:deleteid and id=:messid");
+				$ask->execute(['deleteid' => $deleteid, 'messid' => $deletemsgid ]);
 			}
 
 //-----------------------------------------------------------------------------
