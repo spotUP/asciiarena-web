834a3f8148a4032800d88e893b457e951eca9b29
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-03-01 11:51:16 +0100

    removed decoding of logos

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 header('Content-Type: text/html; charset=UTF-8');
 $logos = [];
 foreach(fetchAll("SELECT ascii FROM logos ORDER BY RAND() limit 10") as $logo) {
-	$logos[] = '<a href="/" class="logo ascii"><pre style="overflow: hidden;"><span class="magenta">' . utf8_encode($logo->ascii) . '</span></pre></a>';
+	$logos[] = '<a href="/" class="logo ascii"><pre style="overflow: hidden;"><span class="magenta">' . '</span></pre></a>';
 }
 $stars1 = <<<EOD
 __/\__
