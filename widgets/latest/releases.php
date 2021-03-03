6bf4ad13c9e6e8fd73b3e596f2be6a19bda79be6
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-03-03 19:23:47 +0100

    test

--- widgets/latest/releases.php
+++ widgets/latest/releases.php
@@ -11,6 +11,7 @@
 	foreach ($res as $row) {
 		$dirname = explode(".", $row->filename);
 		$filename = BASEDIR . "/collections/{$dirname[0]}/{$row->filename}.diz";
+echo "filename: $filename";
 		if (file_exists($filename) && count($collys) < $columns) {
 			$collys[] = utf8_encode(file_get_contents($filename));
 		}
