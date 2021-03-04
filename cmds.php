547c87f290683bf598dace77696b0997679b17b0
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-03-04 14:39:50 +0100

    fixed color of nick in wall

--- cmds.php
+++ cmds.php
@@ -66,7 +66,7 @@
 				foreach (fetchAll("(SELECT * FROM wallposts WHERE wall_id = :wall_id ORDER BY id DESC LIMIT 13) ORDER BY id ASC", [":wall_id" => (int)$wall]) as $row) { ?>
 					<div class="col-lg-12 d-flex justify-content-between">
 						<pre class="wall"><span class="cyan"><?=$row->tag?></span></pre>
-						<span class="pink"><?=$row->nick?></span>
+						<span class="lightpink"><?=$row->nick?></span>
 					</div>
 				<?php }
 				exit();
