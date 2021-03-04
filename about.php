a548e421b09d8625fd7f29a39099e43f321ce52a
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-03-04 11:38:26 +0100

    added a skeleton about page

---
+++ about.php
@@ -0,0 +1,27 @@
+<?php
+require_once "session.php";
+$h1 = ["About aSCIIaRENA", "bY uP rOUGH and diViNE sTYLERS"];
+include "header.php";
+?>
+<div class="modal-body row m-0 p-0">
+	<div class="col-lg-8 order-md-1 order-lg-2 order-xl-2 m-0 p-0 m-sm-1 p-sm-1">
+		CREDITS
+
+		Idea/Concept/Design/Code by Spot
+		Code: Fred, Hamlet, Origo, Phiwer, Nicomen, Stalvik, TZ, Ziphoid (should be completed with details)
+
+		Admins:
+		Ne7, Zito, Dino, Etc etc.. complete this...
+
+		Thanks to...
+		Yonx, (Complete this...)
+
+	</div>
+	<div class="col-lg-2 order-md-2 order-lg-1 order-xl-1">
+		<?php include "sidebar.php"; ?>
+	</div>
+	<div class="col-lg-2 order-md-3 order-lg-3 order-xl-3">
+		<?php include "sidebar_right.php"; ?>
+	</div>
+</div>
+<?php include "footer.php"; ?>
