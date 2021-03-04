1736bfeca9400124b39b71b1c37cf578bd1f9ee7
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-03-04 09:42:09 +0100

    Fixed layout on broken collys view

--- admin_edit_broken_collys.php
+++ admin_edit_broken_collys.php
@@ -3,27 +3,34 @@
 // BROKEN COLLYS                                                                    
 // -------------------------------------------------------------------------------- 
 ?>
-	<div class="tab-pane fade" id="broken">
+<div class="tab-pane fade" id="broken">
 
-		Broken Collys
+	<form enctype="multipart/form-data" action="#broken" method="post">
+		<?php
+		$ask="SELECT * FROM collys WHERE broken='1' ORDER BY filename ASC";
+		$result=fetchAll($ask);
+		foreach ($result as $row)
+		{
+			$filename=$row->filename;
+			$encoded_filename=base64_encode($filename);
+			$broken_comment=$row->broken_comment;
 
-		<form enctype="multipart/form-data" action="#broken" method="post">
-			<?php
-			$ask="SELECT * FROM collys WHERE broken='1' ORDER BY filename ASC";
-			$result=fetchAll($ask);
-			foreach ($result as $row)
-			{
-				$filename=$row->filename;
-				$encoded_filename=base64_encode($filename);
-				$broken_comment=$row->broken_comment;
-
-				?>
-				<a href="info_release.php?filename=<?=$encoded_filename?>"><?=$filename?></a>
-				<input type="hidden" name="filename" value="<?=$filename?>">
-				<input type="submit" name="colly_fixed" value="Fixed">
-				Comment: <?=$broken_comment?>
-				<?php
-			}
 			?>
-		</form>
-	</div>
+			<div class="row ap-0">
+
+				<div class="col-2">
+					<a href="info_release.php?filename=<?=$encoded_filename?>"><?=$filename?></a>
+				</div>
+				<div class="col-8">
+					Comment: <?=$broken_comment?>
+				</div>
+				<div class="col-2">
+					<input type="hidden" name="filename" value="<?=$filename?>">
+					<input type="submit" name="colly_fixed" value="Fixed">
+				</div>
+			</div>
+			<?php
+		}
+		?>
+	</form>
+</div>
