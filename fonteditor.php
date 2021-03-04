0f0f992dd4b39834e2417ce0cbd85354ae072800
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-03-04 13:54:31 +0100

    changed header

--- fonteditor.php
+++ fonteditor.php
@@ -11,7 +11,7 @@ include "header.php";
     <div class="col-lg-8">
         <div class="row" >
             <div class="col-lg-12">
-                <h2 id="nav-tabs">PRIVATE FONT EDITOR</h2>
+                <h2 class="bg-header" id="nav-tabs">PRIVATE FONT EDITOR</h2>
                 <div class="bs-component">
                     <ul class="nav nav-tabs">
                         <li class="nav-item">
