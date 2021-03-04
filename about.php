6b100c98c96cbc29c259f6604ab88876738a6b4e
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-03-04 12:07:41 +0100

    cleaned up html

--- about.php
+++ about.php
@@ -5,23 +5,27 @@ include "header.php";
 ?>
 <div class="modal-body row m-0 p-0">
 	<div class="col-lg-8 order-md-1 order-lg-2 order-xl-2 m-0 p-0 m-sm-1 p-sm-1">
-
-		aSCIIaRENA<br><br>
+		<span class="white">aSCIIaRENA</span><br><br>
 		<span class="cyan">Code by Hamlet, Nicomen, TZ, Ziphoid, Spot</span><br>
-		<span class="cyan">Concept and Design by Spot</span><br><br><br>
+		<span class="cyan">aSCIIaRENA is using a modified BOOTSTRA.386 bootstrap theme by kristopolous</span><br><br><br>
+		<span class="cyan">Concept/UI/HTML/CSS by Spot</span><br><br><br>
 
-		CYGNUS EDiTOR ONLiNE<br><br>
-		<span class="cyan">Code by  Fred, Phiwer, Origo</span><br>
-		<span class="cyan">UI/HTML/CSS by Spot</span><br><br><br>
+		<span class="white">CYGNUS EDiTOR ONLiNE</span><br><br>
+		<span class="cyan">Code by Fred, Phiwer, Origo</span><br>
+		<span class="cyan">Concept/UI/HTML/CSS by Spot</span><br><br><br>
 
-		HiPPOPLAYER ONLiNE<br><br>
-		<span class="cyan">Code by  Stalvik</span><br>
-		<span class="cyan">UI/HTML/CSS by Spot</span><br>
+		<span class="white">HiPPOPLAYER ONLiNE</span><br><br>
+		<span class="cyan">Code by Stalvik</span><br>
+		<span class="cyan">Concept/UI/HTML/CSS by Spot</span><br>
 		Add refs to player libs here</span><br><br><br>
 
-		ADMiNS<br><br>
-		<span class="cyan">Dino, Dipswitch, h7, Ne7, Skope, Spot, Yonx, Zito</span><br><br><br>
+        <span class="white">ASCII FONT EDITOR</span><br><br>
+		<span class="cyan">Code by ....</span><br>        
+		<span class="cyan">Concept/UI/HTML/CSS by Spot</span><br><br><br>
+		<span class="cyan">Based on a web adaption of FIGLET by PatorJK</span><br><br><br>
 
+		<span class="white">ADMiNS</span><br><br>
+		<span class="cyan">Dino, Dipswitch, h7, Ne7, Skope, Spot, Yonx, Zito</span><br><br><br>
 	</div>
 	<div class="col-lg-2 order-md-2 order-lg-1 order-xl-1">
 		<?php include "sidebar.php"; ?>
