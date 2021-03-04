81feca5cbf79eb435ac99a8487c937cd80a20120
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-03-04 12:02:08 +0100

    cleaned up html

--- fonteditor.php
+++ fonteditor.php
@@ -9,14 +9,6 @@ include "header.php";
         <?php include "sidebar.php"; ?>
     </div>
     <div class="col-lg-8">
-        <?php /* widgets([
-            ["file" => "latest/releases", "header" => "LATEST RELEASES", "columns" => 2],
-            ["file" => "wall", "header" => "WALL OF FAME"]
-        ]); */ ?>
-
-        <!-- app css -->
-        <!--    <link rel="stylesheet" type="text/css" href="assets/css/fonteditor.css" /> -->
-
         <div class="row" >
             <div class="col-lg-12">
                 <h2 id="nav-tabs">PRIVATE FONT EDITOR</h2>
@@ -31,50 +23,32 @@ include "header.php";
                     </ul>
                 </div>
             </div>
-
-
-
-
-
-
-
-
-
-
-
-    <div id="view-container" class="container">
-        <div id='view' 
-        class='page'
-        ng-view="" 
-        ng-cloak
-        ></div>
+            <div id="view-container" class="container">
+                <div id='view' 
+                class='page'
+                ng-view="" 
+                ng-cloak
+                ></div>
+            </div>
+            <div dialog-export></div>
+            <div dialog-import></div>
+            <div dialog-submit-font></div>
+            <script src='/assets/js/fonteditor/vendor/figlet/lib/figlet.js'></script>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+            <script src="https://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+            <script src='https://ajax.googleapis.com/ajax/libs/angularjs/1.2.9/angular.min.js'></script>
+            <script src='https://ajax.googleapis.com/ajax/libs/angularjs/1.2.9/angular-route.min.js'></script>
+            <script src='https://ajax.googleapis.com/ajax/libs/angularjs/1.2.9/angular-animate.min.js'></script>
+            <script src="/assets/js/fonteditor/app.js"></script>
+            <script src="/assets/js/fonteditor/build/services.js"></script>
+            <script src="/assets/js/fonteditor/build/controllers.js"></script>
+            <script src="/assets/js/fonteditor/build/filters.js"></script>
+            <script src="/assets/js/fonteditor/build/directives.js"></script>
+            <script src="/assets/js/fonteditor/build/templates.js"></script>
+        </div>
     </div>
-    <div dialog-export></div>
-    <div dialog-import></div>
-    <div dialog-submit-font></div>
-
-    <script src='/assets/js/fonteditor/vendor/figlet/lib/figlet.js'></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
-    <script src='https://ajax.googleapis.com/ajax/libs/angularjs/1.2.9/angular.min.js'></script>
-    <script src='https://ajax.googleapis.com/ajax/libs/angularjs/1.2.9/angular-route.min.js'></script>
-    <script src='https://ajax.googleapis.com/ajax/libs/angularjs/1.2.9/angular-animate.min.js'></script>
-    <script src="/assets/js/fonteditor/app.js"></script>
-    <script src="/assets/js/fonteditor/build/services.js"></script>
-    <script src="/assets/js/fonteditor/build/controllers.js"></script>
-    <script src="/assets/js/fonteditor/build/filters.js"></script>
-    <script src="/assets/js/fonteditor/build/directives.js"></script>
-    <script src="/assets/js/fonteditor/build/templates.js"></script>
-</div>
-</div>
-
-</html>
-
-
-
-
-<div class="col-lg-2">
-    <?php include "sidebar_right.php"; ?>
-</div>
+    <div class="col-lg-2">
+        <?php include "sidebar_right.php"; ?>
+    </div>
 </div>
 <?php include "footer.php";
