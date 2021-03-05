02bfbdce06733806b6032d80a1af4f0435d00d1b
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-03-05 16:35:11 +0100

    Added animations to the login alert

--- fonteditor.php
+++ fonteditor.php
@@ -54,7 +54,7 @@ include "header.php";
             ?>
             <div class="col-lg-12">
                 <div class="bs-component">
-                    <div class="alert alert-dismissible alert-primary">
+                    <div class="animate__animated animate__shakeX alert alert-dismissible alert-primary">
                         <button type="button" class="close" data-dismiss="alert">x</button>
                         You need to be <a class="ascii" data-toggle="modal" style="padding-right: 8px;" href="#login">logged in</a>to use this feature.
                     </div>
