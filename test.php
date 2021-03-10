1ce2699214d358aafcbc0bb7ed5cb47cf0463b17
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-03-10 21:58:17 +0100

    added testpage

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+require_once "session.php";
+$h1 = ["wELCOME tO aSCIIaRENA", "bY uP rOUGH and diViNE sTYLERS"];
+include "header.php";
+?>
+<div class="modal-body row m-0 p-0">
+	<div class="col-lg-8 order-md-1 order-lg-2 order-xl-2 m-0 p-0 m-sm-1 p-sm-1">
+
+<?php phpinfo()?>
+
+	</div>
+	<div class="col-lg-2 order-md-2 order-lg-1 order-xl-1">
+		<?php include "sidebar.php"; ?>
+	</div>
+	<div class="col-lg-2 order-md-3 order-lg-3 order-xl-3">
+		<?php include "sidebar_right.php"; ?>
+	</div>
+</div>
+<?php include "footer.php"; ?>
