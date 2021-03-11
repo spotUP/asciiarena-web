9905b34f91328fdbf553613d7d7436a38c58ae38
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-03-11 21:47:00 +0100

    Fix missing color

--- info_crew.php
+++ info_crew.php
@@ -383,7 +383,7 @@ include "header.php";
 							{
 								if($c == count($authors)-1) 
 								{
-									echo ' <span class="magenta" &amp; </span>';
+									echo ' <span class="magenta"> &amp; </span>';
 								} 
 								else 
 								{
