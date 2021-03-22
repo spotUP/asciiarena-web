d3e6622bd6abb457860e71dcdbbc4320e3fd6449
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-03-22 22:22:01 +0100

    layout fixes

--- admin_edit_artist.php
+++ admin_edit_artist.php
@@ -21,27 +21,29 @@ if(isset($_POST['getartist']) && is_admin())
 }
 ?>
 <div class="tab-pane fade ap-1" id="artist">
-	<form enctype="multipart/form-data" action="#artist" method="post">
-		<div class="custom-select">
-			<select name="getartist" onchange="this.form.submit();">
-				<?php
-				if (isset($show_artist_nick))
-				{
-					?>
-					<option selected="selected" value="$show_all_user_names"><?=$show_artist_nick?></option>
+	<form action="#artist" method="post">
+		<div class="row apb-1">
+			<div class="col-12">
+				<select name="getartist" class="w-100" onchange="this.form.submit();">
 					<?php
-				}
-				$ask="select nick from artists";
-				$result=fetchAll($ask);
-				foreach ($result as $row)
-				{
-					$show_all_artist_names=$row->nick;
+					if (isset($show_artist_nick))
+					{
+						?>
+						<option selected="selected" value="$show_all_user_names"><?=$show_artist_nick?></option>
+						<?php
+					}
+					$ask="select nick from artists";
+					$result=fetchAll($ask);
+					foreach ($result as $row)
+					{
+						$show_all_artist_names=$row->nick;
+						?>
+						<option><?=$show_all_artist_names?></option>
+						<?php
+					}
 					?>
-					<option><?=$show_all_artist_names?></option>
-					<?php
-				}
-				?>
-			</select>
+				</select>
+			</div>
 		</div>
 	</form>
 	<form enctype="multipart/form-data" action="#" method="post">
@@ -51,78 +53,111 @@ if(isset($_POST['getartist']) && is_admin())
 			$getartist=$_POST['getartist'];
 			?>
 
-			Nick
-			<input type="text" size="20" name="edit_artist_nick" value="<?=$show_artist_nick?>">
-
-			Acronym
-			<input type="text" size="20" name="edit_artist_acronym" value="<?=$show_artist_acronym?>">
-
-			Webpage
-			<input type="text" size="20" name="edit_artist_www" value="<?=$show_artist_www?>">
-
-			Crew
-			<?php
-			$ask="select crew from member_of where nick='$getartist'";
-			$result=fetchAll($ask);
-			foreach ($result as $row)
-			{
-				$artist_crew=$row->crew;
-				?>
-				<select name="old_artist_crews[]"> 
-					<option selected="selected"><?=$artist_crew?></option>
-					<option value="Delete">Remove Crew</option>
-					<?php
-					$ask_crews="select name from crews";
-					$result_crews=fetchAll($ask_crews);
-					foreach ($result_crews as $row_crews)
-					{
-						$crews=$row_crews->name;
-						?><option><?=$crews?></option><?php
-					}
-					?>
-				</select>
+			<div class="row apb-1">
+				<div class="col-6">
+					<span class="white">Nick:</span>
+					<input type="text" size="20" name="edit_artist_nick" value="<?=$show_artist_nick?>">
+				</div>
+			</div>
+			<div class="row apb-1">
+				<div class="col-6">
+					Acronym
+				</div>
+				<div class="col-6">
+					<input type="text" size="20" name="edit_artist_acronym" value="<?=$show_artist_acronym?>">
+				</div>
+			</div>
+			<div class="row apb-1">
+				<div class="col-6">
+					<span class="white">Webpage</span>
+				</div>
+				<div class="col-6">
+					<input type="text" size="20" name="edit_artist_www" value="<?=$show_artist_www?>">
+				</div>
+			</div>
+			<div class="row apb-1">
+				<div class="col-6">
+					<span class="white">Crew</span>
+				</div>
 				<?php
-			}
-			?>
-
+				$ask="select crew from member_of where nick='$getartist'";
+				$result=fetchAll($ask);
+				foreach ($result as $row)
+				{
+					$artist_crew=$row->crew;
+					?>
+					<div class="col-6">
+						<select name="old_artist_crews[]"> 
+							<option selected="selected"><?=$artist_crew?></option>
+							<option value="Delete">Remove Crew</option>
+							<?php
+							$ask_crews="select name from crews";
+							$result_crews=fetchAll($ask_crews);
+							foreach ($result_crews as $row_crews)
+							{
+								$crews=$row_crews->name;
+								?><option><?=$crews?></option><?php
+							}
+							?>
+						</select>
+					</div>
+					<?php
+				}
+				?>
+			</div>
 			<span id="new_artist_crew_field"></span> 
 			<span onclick="add_artist_crew_field();" style="cursor: pointer; cursor: hand;"><button type="button">Add Crew!</button></span>
 			<input type="hidden" name="total_artist_crews" id="total_artist_crews" value="0">
 
-			Country:
-			<select name="change_artist_country">
-				<?php
-				if (!empty($show_artist_country))
-				{
-					?>
-					<option selected value="$symbol"><?=$show_artist_country?></option>
-					<?php
-				}
-				else
-				{
-					?>
-					<option selected value="<?=$symbol?>">Unknown</option>
-					<?php					
-				}
-				foreach($country_list as $symbol => $country)
-				{
-					?>
-					<option value="<?=$symbol?>">$country</option>
-					<?php
-				}
-				?>
-			</select>
-
-			Status:
-			<select name="edit_artist_status">
-				<option selected="selected"><?=$show_artist_status?></option>
-				<option>Yes</option>
-				<option>No</option>
-			</select>
-			<input type="hidden" name="getartist" value="<?=$getartist?>" />	
-			<input type="submit" name="delete_artist" value="Delete">
-			<input type="hidden" name="getartist" value="<?=$getartist?>" />	
-			<input type="submit" name="do_edit_artist" value="Change">
+			<div class="row apb-1">
+				<div class="col-6">
+					<span class="white">Country:</span>
+				</div>
+				<div class="col-6">
+					<select name="change_artist_country">
+						<?php
+						if (!empty($show_artist_country))
+						{
+							?>
+							<option selected value="$symbol"><?=$show_artist_country?></option>
+							<?php
+						}
+						else
+						{
+							?>
+							<option selected value="<?=$symbol?>">Unknown</option>
+							<?php					
+						}
+						foreach($country_list as $symbol => $country)
+						{
+							?>
+							<option value="<?=$symbol?>">$country</option>
+							<?php
+						}
+						?>
+					</select>
+				</div>
+			</div>
+			<div class="row apb-1">
+				<div class="col-6">
+					<span class="white">Status:</span>
+				</div>
+				<div class="col-6">
+					<select name="edit_artist_status">
+						<option selected="selected"><?=$show_artist_status?></option>
+						<option>Yes</option>
+						<option>No</option>
+					</select>
+				</div>
+			</div>
+			<div class="row apb-1">
+				<div class="col-12">
+					<input type="hidden" name="getartist" value="<?=$getartist?>" />	
+					<input type="submit" name="delete_artist" value="Delete">
+					<input type="hidden" name="getartist" value="<?=$getartist?>" />	
+					<input type="submit" name="do_edit_artist" value="Change">
+				</div>
+			</div>
 			<?php
 		} 
 		?>
