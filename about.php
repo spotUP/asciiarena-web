6899e8fc2cd7bf2ac7f9af7ce03c49523aa2a30e
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-03-29 10:15:17 +0200

    Added facebook button

--- about.php
+++ about.php
@@ -6,7 +6,7 @@ include "header.php";
 <div class="modal-body row m-0 p-0">
 	<div class="col-lg-8 order-md-1 order-lg-2 order-xl-2 m-0 p-0 m-sm-1 p-sm-1">
 		<span class="white">aSCIIaRENA</span><br><br>
-		<span class="cyan">Code by Hamlet, Nicomen, Spot, TZ, Ziphoid</span><br>
+		<span class="cyan">Code by Burps, Hamlet, Nicomen, Spot, TZ, Ziphoid</span><br>
 		<span class="cyan">aSCIIaRENA is using a modified BOOTSTRA.386 bootstrap theme by Kristopolous</span><br><br><br>
 		<span class="cyan">Concept/UI/HTML/CSS by Spot</span><br><br><br>
 
@@ -26,6 +26,9 @@ include "header.php";
 
 		<span class="white">ADMiNS</span><br><br>
 		<span class="cyan">Dino, Dipswitch, h7, Ne7, Skope, Spot, Yonx, Zito</span><br><br><br>
+
+		<span class="white">FOLLOW</span><br><br><br>
+		<span class="white bg-blue ap-1"><a href="https://www.facebook.com/asciiarena">Facebook</a></span><br><br><br>
 	</div>
 	<div class="col-lg-2 order-md-2 order-lg-1 order-xl-1">
 		<?php include "sidebar.php"; ?>
