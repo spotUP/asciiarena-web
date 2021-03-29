fc4fad67eebd15723038e54ebe601179bd214b30
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-03-29 12:02:02 +0200

    minor layout fix

--- widgets/arena_stats.php
+++ widgets/arena_stats.php
@@ -3,18 +3,18 @@
 	<h2 class="ap-1 bg-header">aSCIIaRENA STATS</h2>
 </div>
 <div class="col-12 d-flex justify-content-between">
-	<span>Collys Online:</span>
+	<span class="white">Collys Online:</span>
 	<span><?=fetchOne("SELECT COUNT(*) total FROM collys")->total?></span>
 </div>
 <div class="col-12 d-flex justify-content-between">
-	<span>Pumped Bytes:</span>
+	<span class="white">Pumped Bytes:</span>
 	<span><?=fetchOne("SELECT sum(filesize) bytes FROM collys")->bytes?></span>
 </div>
 <div class="col-12 d-flex justify-content-between">
-	<span>Users:</span>
+	<span class="white">Users:</span>
 	<span><?=fetchOne("SELECT COUNT(*) users FROM users")->users?></span>
 </div>
 <div class="col-12 d-flex justify-content-between">
-	<span>Comments:</span>
+	<span class="white">Comments:</span>
 	<span><?=fetchOne("SELECT COUNT(*) comments FROM comments")->comments?></span>
 </div>
