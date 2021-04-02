86b461673199b14a91ed5343dd792b66a26b24f8
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-04-02 23:28:05 +0200

    removed access control

--- fonteditor.php
+++ fonteditor.php
@@ -6,8 +6,8 @@ include "header.php";
 <div class="modal-body row m-0 p-0">
     <div class="col-lg-8 order-md-1 order-lg-2 order-xl-2 m-0 p-0 m-sm-1 p-sm-1">
         <?php
-        if (is_logged_in()) 
-        {
+ //       if (is_logged_in()) 
+ //       {
             ?>
             <html ng-app="figfont">
             <div class="row" >
@@ -44,7 +44,7 @@ include "header.php";
                 <script src="/assets/js/fonteditor/build/templates.js"></script>
             </div>
             <?php
-        }
+/*        }
         else
         {
             ?>
@@ -57,7 +57,7 @@ include "header.php";
                 </div>
             </div>
             <?php
-        }
+        } */
         ?>
     </div>
     <div class="col-lg-2 order-md-2 order-lg-1 order-xl-1">
