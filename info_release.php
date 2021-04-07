5d8727390a9975519f4f0a9aac8fc7d11c5d6a11
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-04-08 00:28:26 +0200

    added padding

--- info_release.php
+++ info_release.php
@@ -856,8 +856,7 @@ if (isset($_POST[ 'view' ]) || (isset($_POST[ 'change' ])))
 	{
 		?>
 		<div class="row ml-0 mr-0 amb-1 p-0 xs-m-0 xs-m-0 xs-p-0 s-m-0 justify-content-center align-items-center" style="background-color: #000;"> 
-		<br><br><br><br><br>
-			<div id="colly"></div>
+			<div id="colly" style="padding-top: 64px;"></div>
 		</div>
 		<script type="text/javascript" src="/assets/js/ansilove.js"></script>
 		<script>
