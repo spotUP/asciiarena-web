1b48dc5d28d99672141db7decbcee752cce0174d
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-04-08 21:03:09 +0200

    fixed widget error

--- widgets/latest/added_apps.php
+++ widgets/latest/added_apps.php
@@ -1,28 +1,33 @@
 <?php defined('VALID') or die('Nuh-uh!'); ?>
 <div class="container fluid">
 	<div class="header col-lg-12 m-0 p-0">
-		<h2 class="ap-1 bg-header"><a class="lightgreen" href="/apps.php?sort_by=timestamp">LATEST ADDED APPS</a> <a class="lightgreen"		                                                                                     href="/rss.php?class=0">[RSS]</a></h2>
+		<h2 class="ap-1 bg-header"><a class="lightgreen" href="/apps.php?sort_by=timestamp">LATEST ADDED APPS</a> <a class="lightgreen"	href="/rss.php?class=0">[RSS]</a></h2>
 	</div>
 	<div class="container m-0 p-0 apt-1 apb-1 bg-secondary">
 		<?php
-		if (in_array($_user[ "rank" ], ["Elite", "Admin"])) {
+		if (in_array($_user[ "rank" ], ["Elite", "Admin"])) 
+		{
 			$ask = "SELECT * FROM apps ORDER BY timestamp DESC LIMIT 5";
-		} else {
+		} 
+		else 
+		{
 			$ask = "SELECT * FROM apps WHERE status!='Illegal' ORDER BY timestamp DESC LIMIT 5";
 		}
-		foreach (fetchAll($ask) as $row) {
+		foreach (fetchAll($ask) as $row) 
+		{
 			$filename = $row->filename;
-		$filename = str_replace("&#39;", "'", $filename);        // replace ' with &#39
-		$filename = myTruncate($filename, 12);            // truncate
-		$filename = str_replace("'", "&#39;", $filename);        // replace ' with &#39
-		$upload_date = $row->timestamp;
-		$upload_date = date("y-m-d", $upload_date);
-		?>
-		<div class="col-lg-12 d-flex justify-content-between">
-			<a class="magenta" href="/apps.php?sort_by=timestamp DESC"><?=$filename?></a>
-			<?=$upload_date?>
-		</div>
-		<?php}
+			$filename = str_replace("&#39;", "'", $filename);        // replace ' with &#39
+			$filename = myTruncate($filename, 12);            // truncate
+			$filename = str_replace("'", "&#39;", $filename);        // replace ' with &#39
+			$upload_date = $row->timestamp;
+			$upload_date = date("y-m-d", $upload_date);
+			?>
+			<div class="col-lg-12 d-flex justify-content-between">
+				<a class="magenta" href="/apps.php?sort_by=timestamp DESC"><?=$filename?></a>
+				<?=$upload_date?>
+			</div>
+			<?php
+//		}
 		?>
 	</div>
 </div>
