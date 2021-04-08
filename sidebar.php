327a46b5df164efea5823adf8fad117563a341d4
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-04-08 21:18:23 +0200

    fixed widget layout

--- sidebar.php
+++ sidebar.php
@@ -2,7 +2,8 @@
 	widgets([
 		"message_alerts",
 		"users_online",
-		["file" => "last_callers", "header" => "LAST CALLERS", "limit" => 5],
+//		["file" => "last_callers", "header" => "LAST CALLERS", "limit" => 5],
+		["file" => "last_callers", "limit" => 5],
 		"latest/released_collys",
 		"latest/added_collys",
 		"latest/added_mags",
--- widgets/last_callers.php
+++ widgets/last_callers.php
@@ -1,13 +1,20 @@
+<?php defined('VALID') or die('Nuh-uh!'); ?>
 <div class="container fluid bg-secondary">
-	<?php defined('VALID') or die('Nuh-uh!');
-	foreach (fetchAll("SELECT user_id, nick, timestamp FROM lastusers ORDER BY timestamp DESC LIMIT {$limit}") as $row) 
-	{ 
-		?>
-		<div class="col-lg-12 d-flex justify-content-between m-0 p-0">
-			<a class="yellow" href="/member/<?=urlsafe($row->nick)?>"><?=$row->nick?></a>
-			<?=Date("H:i", $row->timestamp)?>
+	<div class="header col-lg-12 m-0 p-0">
+		<h2 class="ap-1 bg-header"><a class="lightgreen" href="/apps.php?sort_by=timestamp">LATEST ADDED APPS</a> <a class="lightgreen"	href="/rss.php?class=0">[RSS]</a></h2></div>
+		<div class="container m-0 p-0 apt-1 apb-1 bg-secondary">
+
+			<?php
+			foreach (fetchAll("SELECT user_id, nick, timestamp FROM lastusers ORDER BY timestamp DESC LIMIT {$limit}") as $row) 
+			{ 
+				?>
+				<div class="col-lg-12 d-flex justify-content-between m-0 p-0">
+					<a class="yellow" href="/member/<?=urlsafe($row->nick)?>"><?=$row->nick?></a>
+					<?=Date("H:i", $row->timestamp)?>
+				</div>
+				<?php
+			}
+			?>
 		</div>
-		<?php
-	}
-	?>
+	</div>
 </div>
