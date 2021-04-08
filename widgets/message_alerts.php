5f6f3e5b5d78b9451b947b4fb51ea469794603d2
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-04-08 21:55:04 +0200

    layout fixes

--- widgets/message_alerts.php
+++ widgets/message_alerts.php
@@ -6,8 +6,7 @@ if (!$messages) {
 ?>
 
 <div class="bs-component col-12">
-	<div class="animate__animated animate__bounce animate__delay-2s alert alert-dismissible alert-primary bg-secondary">
-		<button type="button" class="close" data-dismiss="alert">x</button>
+	<div class="animate__animated animate__bounce animate__delay-2s alert alert-primary bg-secondary">
 		<a class="cyan" href="messages.php">You have <?=$messages?> new message<?=($messages > 1) ? "s" : ""?></a>
 	</div>
 </div>
