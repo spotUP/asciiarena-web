0bb3f47a1da072dd29482552785010e660454967
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-04-10 22:21:44 +0200

    test

--- info_release.php
+++ info_release.php
@@ -704,7 +704,7 @@ require_once "header.php"; ?>
             $row = fetchOne("SELECT view_counter, type FROM collys WHERE filename = :filename", [":filename" => $filename]);
             $downloads = $row->downloads+1;
 
-echo "$_SERVER['PHP_SELF'] . $filenameandpath";
+echo $_SERVER['PHP_SELF'] . $filenameandpath;
 
             doQuery("update collys set downloads=:downloads where filename=:filename", [":downloads" => $downloads, ":filename" => $filename]);
             ?><meta content="3"; URL="<?=$_SERVER['PHP_SELF'] . $filenameandpath?>" http-equiv="Refresh"><?php
