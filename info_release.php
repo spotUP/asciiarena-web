69cd2dd363907e3950f4a3ae8524773600bef661
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-04-10 22:14:52 +0200

    test

--- info_release.php
+++ info_release.php
@@ -705,8 +705,7 @@ require_once "header.php"; ?>
             $downloads = $row->downloads+1;
 
             doQuery("update collys set downloads=:downloads where filename=:filename", [":downloads" => $downloads, ":filename" => $filename]);
-echo "filenameandpath $filenameandpath"collections/r-hl/r-hl.txt
-            ?><meta content="10"; URL="/<?=$filenameandpath?>" http-equiv="Refresh"><?php
+            ?><meta content="10"; URL="$_SERVER['PHP_SELF']<?=$filenameandpath?>" http-equiv="Refresh"><?php
         }
 
         $font = fetchOne("SELECT def_font FROM users WHERE nick = :nick", [":nick" => $nick]);
