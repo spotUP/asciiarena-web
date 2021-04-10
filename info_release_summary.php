57a0d7760ee4069f259910de22419de235cd0104
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-04-10 15:51:11 +0200

    layout fixes

--- info_release_summary.php
+++ info_release_summary.php
@@ -38,12 +38,12 @@ $dirname = $dirname[ 0 ];
 							<?php if ($colly->file_id == "file_id.diz.png") {
 								$file_id_diz = file_get_contents(__DIR__ . "/collections/file_id.diz.txt");
 								
-								echo "<pre class=\"magenta apt-1\"";
+								echo "<pre style=\"color: red\">";
 								echo $file_id_diz;
 								echo "</pre>";
 							} else {
 								if (file_exists("$file_id")) {
-									echo "<pre class=\"magenta apt-1\">";
+									echo "<pre class=\"magenta\">";
 									echo utf8_encode($file_id_diz);
 									echo "</pre>";
 								}
