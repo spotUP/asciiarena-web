1a5a08a5695a738e032c4d760fd9c719edcedf38
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-04-13 13:19:13 +0200

    revert

--- info_release.php
+++ info_release.php
@@ -832,7 +832,6 @@ if (isset($_POST[ 'view' ]) || (isset($_POST[ 'change' ])))
 		if (file_exists(__DIR__ . "/collections/{$dirname}/{$filename}")) 
 		{
 			$content = file_get_contents(__DIR__ . "/collections/{$dirname}/{$filename}");
-            $content = htmlspecialchars($content);
 			echo "<br><br><br><br>";
 			echo utf8_encode($content);
 			echo "<br><br><br><br>";
