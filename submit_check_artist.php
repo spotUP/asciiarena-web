750b4c62fac1edf2148c99b0e76ed3a281b8a3a0
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-04-14 17:32:08 +0200

    fixed notification

--- submit_check_artist.php
+++ submit_check_artist.php
@@ -58,41 +58,49 @@ if(isset($_POST['artistnick']))
 		<?php include "sidebar_right.php"; ?>
 	</div>
 </div>
-<?php include "footer.php"; ?>
-		<?php
-		exit();
-	}
+<?php
+include "footer.php";
+exit();
+}
 
-	$ask="insert into artists (nick, www, active, country, rating, acronym, user_id, artisturl) values (:artistnick, :artistwww, :artiststatus, :artistcountry, 0, :artistacronym, :user_id, :artisturl)";
-	doQuery($ask, [
-		'artistnick' => $artistnick,
-		'artistwww' => $artistwww,
-		'artiststatus' => $artiststatus,
-		'artistcountry' => $country_list[$artistcountry],
-		'artistacronym' => $artistacronym,
-		'user_id' => $_user[ "id" ],
-		'artisturl' => urlsafe($artistnick),
-	]);
+$ask="insert into artists (nick, www, active, country, rating, acronym, user_id, artisturl) values (:artistnick, :artistwww, :artiststatus, :artistcountry, 0, :artistacronym, :user_id, :artisturl)";
+doQuery($ask, [
+	'artistnick' => $artistnick,
+	'artistwww' => $artistwww,
+	'artiststatus' => $artiststatus,
+	'artistcountry' => $country_list[$artistcountry],
+	'artistacronym' => $artistacronym,
+	'user_id' => $_user[ "id" ],
+	'artisturl' => urlsafe($artistnick),
+]);
 
-	if (isset($_POST[artist_crew]))
+if (isset($_POST[artist_crew]))
+{
+	foreach($_POST[artist_crew] as $artist_crew)
 	{
-		foreach($_POST[artist_crew] as $artist_crew)
-		{
-			$ask="insert into member_of (crew, nick) values (:artist_crew,:artistnick)";
-			doQuery($ask, ['artist_crew' => $artist_crew, 'artistnick' => $artistnick ]);
-		}
+		$ask="insert into member_of (crew, nick) values (:artist_crew,:artistnick)";
+		doQuery($ask, ['artist_crew' => $artist_crew, 'artistnick' => $artistnick ]);
 	}
-	?>		
-	<div class="headline">
-		Status
+}
+?>		
+		?>
+		<div class="bs-component">
+			<div class="animate__animated animate__tada alert alert-dismissible alert-success">
+				<button type="button" class="close" data-dismiss="alert">x</button>
+				<span>The artist has been posted!</span>
+			</div>
+		</div>
+		<meta http-equiv="Refresh" content="4"; url="submit.php">
 	</div>
-
-	<div class="content_with_blenk">
-		The artist has been posted!
+	<div class="col-lg-2 order-md-2 order-lg-1 order-xl-1">
+		<?php include "sidebar.php"; ?>
 	</div>
-
-	<meta http-equiv="Refresh" content="1"; url="submit.php">
-	<?php	
-	exit;
+	<div class="col-lg-2 order-md-3 order-lg-3 order-xl-3">
+		<?php include "sidebar_right.php"; ?>
+	</div>
+</div>
+<?php
+include "footer.php";
+exit();
 }
 ?>
