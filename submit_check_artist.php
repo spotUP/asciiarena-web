3af49d74f0cdf9c06dcb53fc4de593762d0ed82b
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-04-14 17:18:01 +0200

    fixed alert position

--- submit_check_artist.php
+++ submit_check_artist.php
@@ -5,90 +5,90 @@
 //---------------------------------------------------------------------------------------------------------------
 //
 
-	if(isset($_POST['artistnick']))
+if(isset($_POST['artistnick']))
+{
+	$artistnick=$_POST['artistnick'];
+	$artistwww=$_POST['artistwww'];
+	$artiststatus=$_POST['artiststatus'];
+	$artistcrew=$_POST['artistcrew'];
+	$artistcountry=$_POST['artistcountry'];
+	$artistacronym=$_POST['artistacronym'];
+
+	$artistnick=cleanInsert($artistnick);
+	$artiststatus=cleanInsert($artiststatus);
+	$artistcrew=cleanInsert($artistcrew);
+	$artistacronym=cleanInsert($artistacronym);
+
+	$ask="SELECT nick from artists WHERE nick=:artistnick";
+	$result=fetchAll($ask, [ 'artistnick' => $artistnick ]);
+	foreach ($result as $row)
 	{
-		$artistnick=$_POST['artistnick'];
-		$artistwww=$_POST['artistwww'];
-		$artiststatus=$_POST['artiststatus'];
-		$artistcrew=$_POST['artistcrew'];
-		$artistcountry=$_POST['artistcountry'];
-		$artistacronym=$_POST['artistacronym'];
+		$artist_dupe=$row[0];				
 
-		$artistnick=cleanInsert($artistnick);
-		$artiststatus=cleanInsert($artiststatus);
-		$artistcrew=cleanInsert($artistcrew);
-		$artistacronym=cleanInsert($artistacronym);
-
-		$ask="SELECT nick from artists WHERE nick=:artistnick";
-		$result=fetchAll($ask, [ 'artistnick' => $artistnick ]);
-		foreach ($result as $row)
-		{
-			$artist_dupe=$row[0];				
-
-			if (strcasecmp($artistnick, $artist_dupe) == 0) 
-			{
-				?>
-		?>
-		<div class="content">
-			<br><br>
-		</div>
-		<div class="bs-component">
-			<div class="animate__animated animate__tada alert alert-dismissible alert-danger">
-				<button type="button" class="close" data-dismiss="alert">x</button>
-				<span>The artist already exists!</span>
-			</div>
-		</div>
-		<meta http-equiv="Refresh" content="4"; url="submit.php">
-		<?php
-		exit();
-			}
-		}
-
-		if (empty($artistnick))
+		if (strcasecmp($artistnick, $artist_dupe) == 0) 
 		{
 			?>
-			<div class="headline">
-				Error
+			?>
+			<div class="content">
+				<br><br>
 			</div>
-
-			<div class="content_with_blenk">
-				You must fill the artist nick field!
+			<div class="bs-component">
+				<div class="animate__animated animate__tada alert alert-dismissible alert-danger">
+					<button type="button" class="close" data-dismiss="alert">x</button>
+					<span>The artist already exists!</span>
+				</div>
 			</div>
-
+			<meta http-equiv="Refresh" content="4"; url="submit.php">
 			<?php
-			exit;
+			exit();
 		}
+	}
 
-		$ask="insert into artists (nick, www, active, country, rating, acronym, user_id, artisturl) values (:artistnick, :artistwww, :artiststatus, :artistcountry, 0, :artistacronym, :user_id, :artisturl)";
-		doQuery($ask, [
-			'artistnick' => $artistnick,
-			'artistwww' => $artistwww,
-			'artiststatus' => $artiststatus,
-			'artistcountry' => $country_list[$artistcountry],
-			'artistacronym' => $artistacronym,
-			'user_id' => $_user[ "id" ],
-			'artisturl' => urlsafe($artistnick),
-		]);
-
-		if (isset($_POST[artist_crew]))
-		{
-			foreach($_POST[artist_crew] as $artist_crew)
-			{
-				$ask="insert into member_of (crew, nick) values (:artist_crew,:artistnick)";
-				doQuery($ask, ['artist_crew' => $artist_crew, 'artistnick' => $artistnick ]);
-			}
-		}
-		?>		
+	if (empty($artistnick))
+	{
+		?>
 		<div class="headline">
-			Status
+			Error
 		</div>
 
 		<div class="content_with_blenk">
-			The artist has been posted!
+			You must fill the artist nick field!
 		</div>
-
-		<meta http-equiv="Refresh" content="1"; url="submit.php">
-		<?php	
+		<meta http-equiv="Refresh" content="4"; url="submit.php">
+		<?php
 		exit;
 	}
+
+	$ask="insert into artists (nick, www, active, country, rating, acronym, user_id, artisturl) values (:artistnick, :artistwww, :artiststatus, :artistcountry, 0, :artistacronym, :user_id, :artisturl)";
+	doQuery($ask, [
+		'artistnick' => $artistnick,
+		'artistwww' => $artistwww,
+		'artiststatus' => $artiststatus,
+		'artistcountry' => $country_list[$artistcountry],
+		'artistacronym' => $artistacronym,
+		'user_id' => $_user[ "id" ],
+		'artisturl' => urlsafe($artistnick),
+	]);
+
+	if (isset($_POST[artist_crew]))
+	{
+		foreach($_POST[artist_crew] as $artist_crew)
+		{
+			$ask="insert into member_of (crew, nick) values (:artist_crew,:artistnick)";
+			doQuery($ask, ['artist_crew' => $artist_crew, 'artistnick' => $artistnick ]);
+		}
+	}
+	?>		
+	<div class="headline">
+		Status
+	</div>
+
+	<div class="content_with_blenk">
+		The artist has been posted!
+	</div>
+
+	<meta http-equiv="Refresh" content="1"; url="submit.php">
+	<?php	
+	exit;
+}
 ?>
