44d95767a4cf88842edff12133be9c926b381415
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-04-14 17:28:14 +0200

    test

--- submit_check_artist.php
+++ submit_check_artist.php
@@ -50,6 +50,14 @@ if(isset($_POST['artistnick']))
 			</div>
 		</div>
 		<meta http-equiv="Refresh" content="4"; url="submit.php">
+	<div class="col-lg-2 order-md-2 order-lg-1 order-xl-1">
+		<?php include "sidebar.php"; ?>
+	</div>
+	<div class="col-lg-2 order-md-3 order-lg-3 order-xl-3">
+		<?php include "sidebar_right.php"; ?>
+	</div>
+</div>
+<?php include "footer.php"; ?>
 		<?php
 		exit();
 	}
