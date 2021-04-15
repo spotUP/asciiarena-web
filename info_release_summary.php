978b72d61ba883b285f8ee226877d926df272887
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-04-15 15:27:02 +0200

    Trying to fix encoding issues in file_ids

--- info_release_summary.php
+++ info_release_summary.php
@@ -37,8 +37,8 @@ foreach (fetchAll("SELECT * FROM collys WHERE filename = :filename", [":filename
 							echo "</pre>";
 						} else {
 							if (file_exists("$file_id")) {
-//								$file_id_diz = htmlspecialchars_decode($file_id_diz, ENT_QUOTES);
-								$file_id_diz = cleanStr($file_id_diz);
+								$file_id_diz = htmlspecialchars_decode($file_id_diz, ENT_QUOTES);
+//								$file_id_diz = cleanStr($file_id_diz);
 								echo "<pre class=\"magenta apt-1\">";
 								echo utf8_encode($file_id_diz);
 								echo "</pre>";
