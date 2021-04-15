d60224e4eab4426228d5da2e1299da7c985fd061
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-04-15 15:30:47 +0200

    Trying to fix encoding issues in file_ids

--- functions.php
+++ functions.php
@@ -407,10 +407,16 @@ function urlsafe($string) {
 // FUNCTION: fix text encoding issues
 //-----------------------------------------------------------
 
-function cleanStr($value){ 
-    $value = str_replace('Â', '', $value);
- //   $value = iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', $value);
-    return $value;
+function RemoveBS($Str) {  
+  $StrArr = str_split($Str); $NewStr = '';
+  foreach ($StrArr as $Char) {    
+    $CharNo = ord($Char);
+    if ($CharNo == 163) { $NewStr .= $Char; continue; } // keep £ 
+    if ($CharNo > 31 && $CharNo < 127) {
+      $NewStr .= $Char;    
+    }
+  }  
+  return $NewStr;
 }
 
 //-----------------------------------------------------------
