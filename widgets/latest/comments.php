000c69e334b33fd43da43a037c964650696c7bf3
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-04-20 09:54:54 +0200

    Fix mobile layout - very nice now

--- widgets/latest/comments.php
+++ widgets/latest/comments.php
@@ -6,13 +6,13 @@
 <div class="container-fluid apt-1 p-0 p-lg-2">
 	<div class="col-12 bg-secondary apt-1 apb-1">
 		<div class="row apb-1">
-			<div class="col-7 text-truncate">
+			<div class="col-6 col-sm-7 text-truncate">
 				<span class="white text-truncate">COMMENT</span>
 			</div>
-			<div class="col-3 text-truncate">
+			<div class="col-sm-3 text-truncate d-none d-sm-block">
 				<span class="white">COLLY</span>
 			</div>
-			<div class="col-2 text-truncate">
+			<div class="col-6 col-sm-2 text-truncate">
 				<span class="white float-right">NiCK</span>
 			</div>
 		</div>
@@ -20,13 +20,13 @@
 		foreach(fetchAll("SELECT * FROM comments ORDER BY timestamp DESC LIMIT 10") as $row) {
 			?>
 			<div class="row">
-				<div class="col-7 cyan text-truncate">
+				<div class="col-sm-7 cyan">
 					<a class="cyan" href="/release/<?=$row->filename?>"><?=fixOutputPost($row->comment)?></a>
 				</div>
-				<div class="col-3 text-truncate">
+				<div class="col-6 col-sm-3 mb-4 mb-sm-0 text-truncate">
 					<a class="magenta text-truncate" href="/release/<?=$row->filename?>"><?=$row->filename?></a>
 				</div>
-				<div class="col-2 text-truncate">
+				<div class="col-6 col-sm-2 mb-4 mb-sm-0 text-truncate">
 					<a class="yellow text-truncate float-right" href="/member/<?=$row->nick?>"><?=$row->nick?></a>
 				</div>
 			</div>
