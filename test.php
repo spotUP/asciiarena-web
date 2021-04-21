231b9b5893e9f57a32d28e557b10a7b46ade1e64
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-04-21 20:54:09 +0200

    made a new color selector - checkout test.php

--- test.php
+++ test.php
@@ -5,9 +5,64 @@ include "header.php";
 ?>
 <div class="modal-body row m-0 p-0">
 	<div class="col-lg-8 order-md-1 order-lg-2 order-xl-2 m-0 p-0 m-sm-1 p-sm-1">
+		<?php //phpinfo()?>
+		<div class="bs-docs-example">
+			<select id="colorselector_1" class="ascii">
 
-<?php phpinfo()?>
+				<option value='darkgrey' data-color="#555555">Bright Black</option>
+				<option value='blue' data-color="#5555ff">Bright Blue</option>
+				<option value='magenta' data-color="#ff55ff">Bright Magenta</option>
+				<option value='red' data-color="#ff5555">Bright Red</option>
+				<option value='yellow' data-color="#ffff55">Brigt Yellow</option>
+				<option value='green' data-color="#55ff55">Bright Green</option>
+				<option value='cyan' data-color="#55FFFF">Bright Cyan</option>
+				<option value='white' data-color="#ffffff">White</option>
 
+
+				<option value='black' data-color="#000000">Black</option>
+				<option value='darkblue' data-color="#0000aa">Blue</option>
+				<option value='darkmagenta' data-color="#aa00aa">Magenta</option>
+				<option value='darkred' data-color="#aa0000">Red</option>
+				<option value='brown' data-color="#aa5500">Yellow</option>
+				<option value='darkgreen' data-color="#00aa00">Green</option>
+				<option value='darkcyan' data-color="#00aaaa">Cyan</option>
+				<option value='grey' data-color="#aaaaaa">Grey</option>
+
+			</select>
+		</div>
+
+
+
+
+
+
+
+
+
+		<script>
+			$(function() {
+
+				window.prettyPrint && prettyPrint();
+
+				$('#colorselector_1').colorselector();
+				$('#colorselector_2').colorselector({
+					callback : function(value, color, title) {
+						$("#colorValue").val(value);
+						$("#colorColor").val(color);
+						$("#colorTitle").val(title);
+					}
+				});
+
+				$("#setColor").click(function(e) {
+					$("#colorselector_2").colorselector("setColor", "#008B8B");
+				})
+
+				$("#setValue").click(function(e) {
+					$("#colorselector_2").colorselector("setValue", 18);
+				})
+
+			});
+		</script>
 	</div>
 	<div class="col-lg-2 order-md-2 order-lg-1 order-xl-1">
 		<?php include "sidebar.php"; ?>
