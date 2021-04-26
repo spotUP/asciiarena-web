96b9149328a427237e5d882491dd56b68758438a
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-04-26 16:00:48 +0200

    fix for menu

--- header.php
+++ header.php
@@ -290,7 +290,7 @@ include "header_ascii.php";
 									?>
 									<li class="nav-item dropdown">
 										<a class="nav-link dropdown-toggle ascii yellow" style="padding-right: 8px;" data-toggle="dropdown" href="/admin.php" id="themes">ADMiN<span class="caret" style="padding-right: 8px;"></span></a>
-										<div class="dropdown-menu ascii" aria-labelledby="themes">
+										<div class="dropdown-menu dropdown-menu-fix bg-red ascii" aria-labelledby="themes">
 											<a class="dropdown-item ascii" href="/admin.php#colly">Edit Colly<span style="padding-left: 7px;">       </span></a>
 											<a class="dropdown-item ascii" href="/admin.php#crew">Edit Crew<span style="padding-left: 4px;">    </span></a>
 											<a class="dropdown-item ascii" href="/admin.php#artist">Edit Artist<span style="padding-left: 10px;">     </span></a>
