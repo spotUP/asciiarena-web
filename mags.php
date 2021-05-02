e5aa8505db2f2a89e57b1adc2a97ed63c9ef76ed
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-05-02 22:42:15 +0200

    utf encoded file_id

--- mags.php
+++ mags.php
@@ -102,10 +102,12 @@ require_once "header.php"; ?>
                          $file_id = $row->filename.'.diz';
                          if (file_exists('mags/'.$dirname.'/'.$file_id)) {
                             $display_file_id=file_get_contents('mags/'.$dirname.'/'.$file_id);
+                            $display_file_id=utf8_encode($display_file_id_diz);
                                 ?>
                                 <pre style="overflow: hidden;"><a class="magenta ascii" href="/magazine/<?=$row->filename?>"><?=$display_file_id?></a></pre>
                                 <?php
                         }
+
                         ?>
                 </div>
         </div>
