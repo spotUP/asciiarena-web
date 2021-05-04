4fab0009d1042cfad4067d00c70bfacd8e11df9e
root <bart@crossbar.net>
2021-05-04 19:55:29 +0000

    switch to minified js/css

--- header.php
+++ header.php
@@ -15,11 +15,11 @@ include "header_ascii.php";
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<link rel="icon" href="/favicon.ico" type="image/x-icon">
 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-	<link rel="stylesheet" href="/assets/css/bootstrap.css" media="screen">
+	<link rel="stylesheet" href="/assets/css/bootstrap.min.css" media="screen">
 	<link rel="stylesheet" href="/assets/css/bootstrap-colorselector.css" media="screen">
 	<link rel="stylesheet" href="/assets/css/site.css" media="screen">
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-	<script src="/assets/js/bootstrap.bundle.js"></script>
+	<script src="/assets/js/bootstrap.bundle.min.js"></script>
 	<script src="/assets/js/bootstrap-colorselector.js"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
 
