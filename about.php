6fc99664eab7fcfe9ac6cc5d7c5857843008c3c0
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-05-04 12:46:43 +0200

    updated

--- about.php
+++ about.php
@@ -6,8 +6,11 @@ include "header.php";
 <div class="modal-body row m-0 p-0">
 	<div class="col-lg-8 order-md-1 order-lg-2 order-xl-2 m-0 p-0 m-sm-1 p-sm-1">
 		<span class="white">aSCIIaRENA</span><br><br>
-		<span class="cyan">Code by Burps, Hamlet, Nicomen, Spot, TZ, Ziphoid</span><br>
-		<span class="cyan">aSCIIaRENA is using a modified BOOTSTRA.386 bootstrap theme by Kristopolous</span><br><br><br>
+		<span class="cyan">Code by Burps, Nicomen, Spot, Ziphoid</span><br>
+		<span class="cyan">aSCIIaRENA is using a modified BOOTSTRA.386 bootstrap theme by Kristopolous</span>
+		<span class="cyan">Thanks to TZ and Hamlet for their contributions</span>
+		<br><br><br>
+
 		<span class="cyan">Concept/UI/HTML/CSS by Spot</span><br><br><br>
 
 		<span class="white">CYGNUS EDiTOR ONLiNE</span><br><br>
