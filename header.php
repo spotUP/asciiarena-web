10783f006ceb429ce9658dbbe66ceba1ed0db56d
root <bart@crossbar.net>
2021-05-04 19:33:01 +0000

    removed popperjs (is also part of bootstrap bundle)

--- header.php
+++ header.php
@@ -19,7 +19,6 @@ include "header_ascii.php";
 	<link rel="stylesheet" href="/assets/css/bootstrap-colorselector.css" media="screen">
 	<link rel="stylesheet" href="/assets/css/site.css" media="screen">
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-	<script src="https://unpkg.com/@popperjs/core@2"></script>
 	<script src="/assets/js/bootstrap.bundle.js"></script>
 	<script src="/assets/js/bootstrap-colorselector.js"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
