ea46cf6b5dea7d6deb7e2030775b4b7220d9ddfd
root <bart@crossbar.net>
2021-05-05 08:13:34 +0000

    optimized webfont performance

--- header.php
+++ header.php
@@ -18,6 +18,7 @@ include "header_ascii.php";
 	<link rel="stylesheet" href="/assets/css/bootstrap.min.css" media="screen">
 	<link rel="stylesheet" href="/assets/css/bootstrap-colorselector.css" media="screen">
 	<link rel="stylesheet" href="/assets/css/site.css" media="screen">
+        <link rel="preload" href="/assets/fonts/TopazPlus_a1200.woff2" as="font" type="font/woff2" crossorigin>
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 	<script src="/assets/js/bootstrap.bundle.min.js"></script>
 	<script src="/assets/js/bootstrap-colorselector.js"></script>
