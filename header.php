b5ce32e659f189f353f4b56dc35e7e2e93337b92
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-05-05 21:16:36 +0200

    disabled ascii style editor link

--- header.php
+++ header.php
@@ -95,9 +95,9 @@ include "header_ascii.php";
 					<li class="nav-item">
 						<a class="nav-link ascii apr-1" href="/about.php">ABOUT</a>
 					</li>
-					<li class="nav-item">
+				<!--	<li class="nav-item">
 						<a class="nav-link ascii apr-1" href="/fonteditor.php">ASCII STYLE DESIGNER</a>
-					</li>
+					</li> -->
 					<li class="nav-item dropdown">
 						<a class="nav-link dropdown-toggle ascii" style="padding-right: 8px;" data-toggle="dropdown" href="/admin.php" id="themes">SUBMiT<span class="caret" style="padding-right: 8px;"></span></a>
 						<div class="dropdown-menu ascii" aria-labelledby="themes">
