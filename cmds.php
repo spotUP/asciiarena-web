9ab99f0ea54d7300cbed009a540e7de9140b2132
phantasm4489 <darren_m_coles@yahoo.co.uk>
2021-05-07 11:31:58 +0100

    update info release page to use ajax (#118)

--- cmds.php
+++ cmds.php
@@ -93,6 +93,175 @@
 				exit();
 			}
 			break;
+    case "countdl":
+      if ($is_ajax && is_logged_in()) {
+				$colly = $_POST[ "colly_id" ] ?? $_current[ 1 ] ?? 0;
+				if ($colly > 0) {
+					$status = doQuery("update collys set downloads=downloads+1 where id = :colly_id",[
+            ":colly_id" => (int)$colly,
+          ]);
+					$code = ($status) ? 200 : 400;
+					exit(json_out([
+						"status" => $status,
+						"id" => (int)$colly
+					], $code));
+				}
+				exit(json_out(["status" => false], 400));
+			}
+			break;
+    case "countview":
+      if ($is_ajax && is_logged_in()) {
+				$colly = $_POST[ "colly_id" ] ?? $_current[ 1 ] ?? 0;
+				if ($colly > 0) {
+					$status = doQuery("update collys set view_counter=view_counter+1 where id = :colly_id",[
+            ":colly_id" => (int)$colly,
+          ]);
+					$code = ($status) ? 200 : 400;
+					exit(json_out([
+						"status" => $status,
+						"id" => (int)$colly
+					], $code));
+				}
+				exit(json_out(["status" => false], 400));
+			}
+			break;      
+    case "broken":
+      if ($is_ajax && is_logged_in()) {
+				$colly = $_POST[ "colly_id" ] ?? $_current[ 1 ] ?? 0;
+        $comment = $_POST[ 'comment' ] ?? "";
+				if ($colly > 0) {
+					$status = doQuery("update collys set broken=1, broken_comment=:comment where id = :colly_id",[
+            ":colly_id" => (int)$colly,
+            ":comment" => $comment
+          ]);
+					$code = ($status) ? 200 : 400;
+					exit(json_out([
+						"status" => $status,
+						"id" => (int)$colly
+					], $code));
+				}
+				exit(json_out(["status" => false], 400));
+			}
+			break;  
+    case "getcomments":      
+      if ($is_ajax) {
+				$colly = $_POST[ "colly_id" ] ?? $_current[ 1 ] ?? 0;
+				if ($colly > 0) {
+          $data = [];
+          $comments = fetchAll("SELECT commentid,timestamp,nick,ifnull(rating,'') as rating,comment FROM comments WHERE colly_id = :id", [":id" => $colly]);
+          foreach($comments as $comment) {
+            $data[] = [
+              "id" => (int)$comment->commentid,
+              "time" => date("Y-m-d H:i:s",$comment->timestamp),
+              "nick" => $comment->nick,
+              "rating" => $comment->rating,
+              "comment" => $comment->comment
+            ];
+          }
+          if(!empty($data)) {
+            exit(json_out($data));
+          }
+        }
+        exit(json_out(["status" => false], 404));
+			}
+			break;  
+    case "delcomment":
+      if ($is_ajax && is_logged_in() &&is_admin()) {
+				$id = $_POST[ "comment_id" ] ?? $_current[ 1 ] ?? 0;
+        $colly = $_POST[ "colly_id" ] ?? 0;
+				if ($id > 0) {
+					$status = doQuery("delete from comments where commentid = :commentid",[
+            ":commentid" => (int)$id,
+          ]);
+					$code = ($status) ? 200 : 400;
+          doQuery("update collys set rating=(select avg(rating) AS avg from comments where colly_id=:colly_id and rating>0) where id=:colly_id",[
+            ":colly_id" => (int)$colly,
+          ]);
+          recalculate_ratings();
+					exit(json_out([
+						"status" => $status,
+						"id" => (int)$id
+					], $code));
+				}
+				exit(json_out(["status" => false], 400));
+			}
+			break;         
+    case "editcomment":
+      if ($is_ajax && is_logged_in()) {
+				$id = $_POST[ "comment_id" ] ?? $_current[ 1 ] ?? 0;
+        $comment = $_POST[ 'comment' ] ?? "";
+				if ($id > 0) {
+          if (is_admin()) {
+            $admin=1;
+          } else {
+            $admin=0;
+          }
+					$status = doQuery("update comments set comment = :comment where (:admin = 1 or nick = :nick) and commentid = :commentid",[
+            ":commentid" => (int)$id,
+            ":comment" => $comment,
+            ":nick" => $_user[ "nick" ],
+            ":admin" => $admin
+          ]);
+					$code = ($status) ? 200 : 400;
+					exit(json_out([
+						"status" => $status,
+						"id" => (int)$id
+					], $code));
+				}
+				exit(json_out(["status" => false], 400));
+			}
+			break;         
+    case "addcomment":
+      if ($is_ajax && is_logged_in()) {
+				$colly = $_POST[ "colly_id" ] ?? $_current[ 1 ] ?? 0;
+        $comment = $_POST[ 'comment' ] ?? "";
+        $rating = $_POST[ 'rating' ] ?? "";
+        if (empty($rating)) $rating = null;
+				if ($colly > 0) {
+					$status = doQuery("insert into comments (colly_id, filename, crew, artist, comment, rating, nick, timestamp,user_id) 
+            values (:colly_id,:filename,(select w.name from collys_crews cc LEFT JOIN crews w ON w.id=cc.crew_id where cc.colly_id=:colly_id),(select a.nick from artists_collys ac LEFT JOIN artists a ON a.id=ac.artist_id where ac.colly_id=:colly_id),:comment, :rating, :nick, :time, :user_id)",[
+            ":colly_id" => (int)$colly,
+            ":filename" => $_SESSION['filename'],
+            ":comment" => $comment,
+            ":rating" => $rating,
+            ":nick" => $_user[ "nick" ],
+            ":time" => time(),
+            ":user_id" => $_user[ "id" ],
+          ]);
+					$code = ($status) ? 200 : 400;
+          doQuery("update collys set rating=(select avg(rating) AS avg from comments where colly_id=:colly_id and rating>0) where id=:colly_id",[
+            ":colly_id" => (int)$colly,
+          ]);
+          recalculate_ratings();
+
+					exit(json_out([
+						"status" => $status,
+						"id" => (int)$colly
+					], $code));
+				}
+				exit(json_out(["status" => false], 400));
+			}
+			break;         
+		case "fave":
+			if ($is_ajax && is_logged_in()) {
+				$colly = $_POST[ "colly_id" ] ?? $_current[ 1 ] ?? 0;
+				if ($colly > 0) {
+					$status = doQuery("INSERT INTO favourites (user_id, colly_id, nick, filename)
+          VALUES (:user_id, :colly_id, :nick, :filename)", [
+						":user_id" => $_user[ "id" ],
+						":colly_id" => (int)$colly,
+            ":nick" => $_user[ "nick" ],
+            ":filename" => $_SESSION['filename']
+					]);
+					$code = ($status) ? 200 : 400;
+					exit(json_out([
+						"status" => $status,
+						"id" => (int)$colly
+					], $code));
+				}
+				exit(json_out(["status" => false], 400));
+			}
+			break;
 		case "unfave":
 			if ($is_ajax && is_logged_in()) {
 				$colly = $_POST[ "colly_id" ] ?? $_current[ 1 ] ?? 0;
