63b540472434ee5483270e02333d25d6f3c9ec37
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-05-10 00:32:11 +0200

    fixed layout issue

--- widgets/users_online.php
+++ widgets/users_online.php
@@ -1,12 +1,12 @@
 <?php defined('VALID') or die('Nuh-uh!'); ?>
 <div class="container fluid col-12 p-0 pl-lg-2 pr-lg-2" style="min-height: 160px;">
 	<div class="header col-lg-12 p-0"><h2 class="ap-1 bg-header">USERS ONLINE</h2></div>
-	<div class="container col-12 p-0 p-lg-2 bg-secondary">
+	<div class="container col-12 p-0 m-0 bg-secondary">
 		<?php
 		foreach (fetchAll("SELECT id, nick, lastactive FROM users WHERE lastactive > (UNIX_TIMESTAMP()-300) ORDER BY lastactive DESC") as $row) 
 		{
 			?>
-			<div class="col-lg-12 apt-1 p-0 p-lg-2">
+			<div class="col-lg-12 p-0 p-lg-2">
 				<a class="yellow" href="/member/<?=urlsafe($row->nick)?>"><?=$row->nick?></a>
 			</div>
 			<?php
