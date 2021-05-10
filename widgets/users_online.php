7263dbe687cf646d0e399351723e594a776cb273
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-05-10 20:43:06 +0200

    layout fixes

--- widgets/users_online.php
+++ widgets/users_online.php
@@ -6,7 +6,7 @@
 		foreach (fetchAll("SELECT id, nick, lastactive FROM users WHERE lastactive > (UNIX_TIMESTAMP()-300) ORDER BY lastactive DESC") as $row) 
 		{
 			?>
-			<div class="col-lg-12 p-0 p-lg-2">
+			<div class="col-lg-12 p-0">
 				<a class="yellow" href="/member/<?=urlsafe($row->nick)?>"><?=$row->nick?></a>
 			</div>
 			<?php
