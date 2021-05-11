78548d4fd2cf7e0601c7f90aca8f3178aed69817
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-05-11 22:00:19 +0200

    truncated comments

--- widgets/latest/comments.php
+++ widgets/latest/comments.php
@@ -20,8 +20,8 @@
 		foreach(fetchAll("SELECT * FROM comments ORDER BY timestamp DESC LIMIT 10") as $row) {
 			?>
 			<div class="row">
-				<div class="col-sm-7 cyan">
-					<a class="cyan" style="white-space: pre-wrap;" href="/release/<?=$row->filename?>"><?=($row->comment)?></a>
+				<div class="col-sm-7 cyan text-truncate">
+					<a class="cyan" href="/release/<?=$row->filename?>"><?=($row->comment)?></a>
 				</div>
 				<div class="col-6 col-sm-3 mb-4 mb-sm-0 text-truncate">
 					<a class="magenta text-truncate" href="/release/<?=$row->filename?>"><?=$row->filename?></a>
