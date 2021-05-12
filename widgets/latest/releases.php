aec5eb068eb1cbc46dbcbf0d116114369133c838
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-05-12 16:12:51 +0200

    test

--- widgets/latest/releases.php
+++ widgets/latest/releases.php
@@ -20,6 +20,10 @@ foreach($collys as $fname => $colly)
 	?>
 	<div class="col-12 d-flex justify-content-center col-<?=$breakpoint?><?=(round(12 / $columns))?> overflow-hidden apt-1 apb-1">
 		<div class="row animate__animated animate__backInUp">
+			<pre style="opacity: 1.0; position: absolute; top: 0px;"><a href="/release/<?=$fname?>" class="ascii magenta"><?=$colly?></a></pre>
+			<pre style="opacity: 0.15; position: absolute; top: 10px;"><a href="/release/<?=$fname?>" class="ascii magenta"><?=$colly?></a></pre>
+			<pre style="opacity: 0.05; position: absolute; top: 20px;"><a href="/release/<?=$fname?>" class="ascii magenta"><?=$colly?></a></pre>
+			<pre style="opacity: 0.02; position: absolute; top: 30px;"><a href="/release/<?=$fname?>" class="ascii magenta"><?=$colly?></a></pre>
 			<pre><a href="/release/<?=$fname?>" class="ascii magenta"><?=$colly?></a></pre>
 		</div>
 	</div>
