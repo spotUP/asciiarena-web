420d50bb51b2ab183bd1e80f498be3398c2d3c96
Johan Samuelsson <johan.samuelsson@guardtools.com>
2021-05-17 17:00:29 +0200

    added visitor tracking through plausible

--- header.php
+++ header.php
@@ -25,6 +25,7 @@ include "header_ascii.php";
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 	<script src="/assets/js/bootstrap.bundle.min.js"></script>
 	<script src="/assets/js/bootstrap-colorselector.js"></script>
+	<script async defer data-domain="asciiarena.se" src="http://asciiarena-plausible.caprover.asciiarena.se/js/plausible.js"></script>
   <script type="module">
     import init386 from "/assets/js/386-animation/index.js";
     init386({
