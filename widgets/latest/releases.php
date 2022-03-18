ab7310a8bddac68b2465b20ce27cbb8cbac95d17
phantasm4489 <darren_m_coles@yahoo.co.uk>
2022-03-18 12:09:53 +0000

    add mags to latest releases widget

--- widgets/latest/releases.php
+++ widgets/latest/releases.php
@@ -6,30 +6,43 @@ if(empty($breakpoint)) {
 	$breakpoint = "xl-";
 }
 $breakpoint = rtrim($breakpoint, "-") . "-";
-$collys = [];
-$res = fetchAll("(SELECT 'A' type, filename, year(from_unixtime(timestamp)) fyear,month(from_unixtime(timestamp)) fmonth,dayofmonth(from_unixtime(timestamp)) fday from apps order by id desc limit 20) union (select 'C' type, filename, year fyear, month fmonth, day fday from collys order by id desc limit 20) order by fyear desc, fmonth desc, fday desc");
+$releases = [];
+$res = fetchAll("select * from (
+(SELECT 'A' as type, filename, year(from_unixtime(timestamp)) fyear,month(from_unixtime(timestamp)) fmonth,dayofmonth(from_unixtime(timestamp)) fday from apps order by id desc limit 10)
+union
+(select 'C' as type, filename, year fyear, month fmonth, day fday from collys order by year desc, month desc,day desc limit 10)
+union
+(SELECT 'M' as type, filename, year(from_unixtime(timestamp)) fyear,month(from_unixtime(timestamp)) fmonth,dayofmonth(from_unixtime(timestamp)) fday from mags order by timestamp desc limit 10)
+) a
+order by a.fyear desc, a.fmonth desc, a.fday desc limit 10");
 foreach($res as $row) {
 	$dirname = explode(".", $row->filename);
 
   if(strcmp($row->type,"C")==0) {
   	$file_id = BASEDIR . "/collections/{$dirname[0]}/{$row->filename}.diz";
     $filename = "/release/".$row->filename;
-  } else {
+  } else if(strcmp($row->type,"M")==0) {
+  	$file_id = BASEDIR . "/mags/{$dirname[0]}/{$row->filename}.diz";
+    $filename = "/magazine/".$row->filename;
+  } else if(strcmp($row->type,"A")==0) {
     $file_id = preg_replace('/\\.[^.\\s]{3,4}$/', '', $row->filename).'.diz';
     $file_id ='apps/'.$file_id;
     $filename = "/application/".$row->filename;
+  } else {
+    $file_id ='';
+    $filename = '';
   }
 
-	if((count($collys) < $columns) && file_exists($file_id)) {
-		$collys[ $filename ] = utf8_encode(file_get_contents($file_id));
+	if((count($releases) < $columns) && strlen($file_id)>0 && file_exists($file_id)) {
+		$releases[ $filename ] = utf8_encode(file_get_contents($file_id));
 	}
 }
-foreach($collys as $fname => $colly) 
+foreach($releases as $fname => $release) 
 {
 	?>
 	<div class="col-12 d-flex justify-content-center col-<?=$breakpoint?><?=(round(12 / $columns))?> overflow-hidden apt-1 apb-1">
 		<div class="row animate__animated animate__backInUp">
-			<pre><a href="<?=$fname?>" class="ascii magenta"><?=$colly?></a></pre>
+			<pre><a href="<?=$fname?>" class="ascii magenta"><?=$release?></a></pre>
 		</div>
 	</div>
 	<?php 
