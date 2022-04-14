e2dff04d81e06a8a118546879e43004df3b07d6a
phantasm4489 <darren_m_coles@yahoo.co.uk>
2022-04-14 13:39:11 +0100

    remove global wall

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include "header.php";
 			"message_alerts",
 			["file" => "latest/releases", "header" => "LATEST RELEASES", "columns" => 2],
 			["file" => "wall"],
-      ["file" => "globalwall"],
+//      ["file" => "globalwall"],
 //			["file" => "cedd_sessions"],
 			["file" => "latest/comments"],
 		]); ?>
