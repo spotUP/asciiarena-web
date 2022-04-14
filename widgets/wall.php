169d2a19d67e7c832c98e636a4f00128ca11fc34
phantasm4489 <darren_m_coles@yahoo.co.uk>
2022-04-14 12:29:18 +0100

    add global wall

--- widgets/wall.php
+++ widgets/wall.php
@@ -5,7 +5,7 @@ $form = "form_{$wall_id}";
 $tag = "tag_{$wall_id}";
 ?>
 <div class="header col-12 col-lg-12">
-	<h2 class="apt-1 apb-1 bg-header">TAG THE WALL</h2>
+	<h2 class="apt-1 apb-1 bg-header">TAG THE ASCIIARENA WALL</h2>
 </div>
 <div class="container-fluid p-0 p-lg-2">
 	<div class="row m-0 p-0 bg-secondary apb-1" id="<?=$wall?>"></div>
