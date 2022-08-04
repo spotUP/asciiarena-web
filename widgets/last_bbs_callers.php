0fc3920289fa0a4c42408aae15bcb5c2ab50f76b
phantasm4489 <darren_m_coles@yahoo.co.uk>
2022-08-04 16:31:13 +0100

    add upload/download counters for last callers

--- widgets/last_bbs_callers.php
+++ widgets/last_bbs_callers.php
@@ -27,7 +27,7 @@ if (is_logged_in()) $nick=$_user[ "nick" ];
 
       function callersitemsRead(id,data) {
 
-          $(id).html('<div class="col-lg-12 d-flex apb-1"><span class="col-lg-3 white text-truncate" style="white-space: pre">Username</span><span class="col-lg-3 white text-truncate" style="white-space: pre">BBS</span><span class="col-lg-2 white text-truncate" style="white-space: pre">Date</span><span class="col-lg-1 white text-truncate" style="white-space: pre">Time On</span><span class="col-lg-1 white text-truncate" style="white-space: pre">Time Off</span><span class="col-lg-2 white text-truncate" style="white-space: pre">Actions</span></div>')
+          $(id).html('<div class="col-lg-12 d-flex apb-1"><span class="col-lg-2 white text-truncate" style="white-space: pre">Username</span><span class="col-lg-2 white text-truncate" style="white-space: pre">BBS</span><span class="col-lg-2 white text-truncate" style="white-space: pre">Date</span><span class="col-lg-1 white text-truncate" style="white-space: pre">Time On</span><span class="col-lg-1 white text-truncate" style="white-space: pre">Time Off</span><span class="col-lg-2 white text-truncate" style="white-space: pre">Actions</span><span class="col-lg-1 white text-truncate" style="white-space: pre">Upload</span><span class="col-lg-1 white text-truncate" style="white-space: pre">Dnload</span></div>')
 					
           data.calls.forEach(  function(callersItem) {
             
@@ -37,8 +37,54 @@ if (is_logged_in()) $nick=$_user[ "nick" ];
 						timeon = callersItem.Timeon
 						timeoff = callersItem.Timeoff
 						actions = callersItem.Actions
+
+            var upkb = callersItem.Upload
+              
+              if ((upkb<0) || (upkb>9999)) {
+              upkb =( upkb >> 10) & 0x003fffff
+              if (upkb>999) {
+                var i = upkb >> 10
+                var f = ((upkb & 1023) * 10) >> 10
+                if (i>9) {
+                  var ul = ('     ' + i +'G')
+                } else {
+                  var ul = ('     ' + i + '.'+f+'G')
+                }
+              } else {
+                        var ul = ('     ' + upkb+'M')
+              }
+            } else {
+                     var ul = ('     ' + callersItem.Upload+"K")
+            }
+                
+
+            var dnkb = callersItem.Download
+              
+             if ((dnkb<0) || (dnkb>9999)) {
+              dnkb =( dnkb >> 10) & 0x003fffff
+              if (dnkb>999) {
+               var i = dnkb >> 10
+               var f = ((dnkb & 1023) * 10) >> 10
+               if (i>9) {
+                 var dl = ('     ' + i + 'G')
+               } else {
+                 var dl = ('     ' + i + '.'+f+'G')
+               }
+              } else {
+                      var dl = ('     ' + dnkb+'M')
+              }
+            } else {
+                     var dl = ('     ' + callersItem.Download+"K")
+            }
+
+
+            if (dl.length>9) { dl = '     LOTS' }
+            if (ul.length>9) { ul = '     LOTS' }
+            dl = dl.slice(dl.length - 5)
+            ul = ul.slice(ul.length - 5)
+
 						
-            output = '<div class="col-lg-12 d-flex"><span class="col-lg-3 cyan text-truncate" style="white-space: pre">'+username+'</span><span class="col-lg-3 green text-truncate" style="white-space: pre">'+bbsname+'</span><span class="col-lg-2 white text-truncate" style="white-space: pre">'+dateon+'</span><span class="col-lg-1 cyan text-truncate" style="white-space: pre">'+timeon+'</span><span class="col-lg-1 cyan text-truncate" style="white-space: pre">'+timeoff+'</span><span class="col-lg-2 white text-truncate" style="white-space: pre">'+actions+'</span></div>'
+            output = '<div class="col-lg-12 d-flex"><span class="col-lg-2 cyan text-truncate" style="white-space: pre">'+username+'</span><span class="col-lg-2 green text-truncate" style="white-space: pre">'+bbsname+'</span><span class="col-lg-2 white text-truncate" style="white-space: pre">'+dateon+'</span><span class="col-lg-1 cyan text-truncate" style="white-space: pre">'+timeon+'</span><span class="col-lg-1 cyan text-truncate" style="white-space: pre">'+timeoff+'</span><span class="col-lg-2 white text-truncate" style="white-space: pre">'+actions+'</span><span class="col-lg-1 white text-truncate" style="white-space: pre">'+ul+'</span><span class="col-lg-1 white text-truncate" style="white-space: pre">'+dl+'</span></div>'
 			
             //output = '<font color="#0000ff">¦</font>'+comment+colour+'-'+username+'<font color="#0000ff">¦</font><font color="#ffffff">'+clean2(shortCode)+'<font color="#0000ff">¦</font>\n';
             $(id).append(output)
