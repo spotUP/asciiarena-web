6842911a0ef4bcf20a787b254d883334461851b5
phantasm4489 <darren_m_coles@yahoo.co.uk>
2022-08-25 16:52:15 +0100

    remove " in default user voted comment, add same default to recent comments widget.

--- widgets/latest/comments.php
+++ widgets/latest/comments.php
@@ -17,7 +17,7 @@
 			</div>
 		</div>
 		<?php
-		foreach(fetchAll("SELECT * FROM comments ORDER BY timestamp DESC LIMIT 10") as $row) {
+		foreach(fetchAll('SELECT filename,nick,IFNULL(comment,concat(nick," voted ",rating)) comment FROM comments ORDER BY timestamp DESC LIMIT 10') as $row) {
 			?>
 			<div class="row">
 				<div class="col-sm-7 cyan text-truncate">
