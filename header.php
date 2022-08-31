f5b8e970ea6da71bfd594d7174c4b7a53205464e
Johan Samuelsson <johan.samuelsson@guardtools.com>
2022-09-01 00:21:51 +0200

    added a link to the workbench in the header menu

--- header.php
+++ header.php
@@ -118,6 +118,9 @@ include "header_ascii.php";
 					<li class="nav-item">
 						<a class="nav-link ascii apr-1" href="/styleeditor.php">ASCII STYLE DESIGNER</a>
 					</li>
+					<li class="nav-item">
+						<a class="nav-link ascii apr-1" href="https://workbench.asciiarena.se">WORKBENCH</a>
+					</li>
 					<li class="nav-item dropdown">
 						<a class="nav-link dropdown-toggle ascii" style="padding-right: 8px;" data-toggle="dropdown" href="/admin.php" id="themes">SUBMiT<span class="caret" style="padding-right: 8px;"></span></a>
 						<div class="dropdown-menu ascii" aria-labelledby="themes">
