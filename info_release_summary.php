1a030e8bd27c57f4395d0596f597cde3cd683be5
Johan Samuelsson <johan.samuelsson@guardtools.com>
2022-09-02 14:40:36 +0200

    trying to hide viewed for collys of type archive

--- info_release_summary.php
+++ info_release_summary.php
@@ -61,107 +61,123 @@ $dirname = $dirname[0];
 						foreach (fetchAll("SELECT a.nick FROM collys c LEFT JOIN artists_collys ac ON ac.colly_id=c.id LEFT JOIN artists a ON a.id=ac.artist_id 
 							WHERE c.filename=:filename", [":filename" => $filename]) as $row) {
 							$author = $row->nick;
-							$authors[] = "<a class=\"green\" href=\"/artist/".urlsafe($author)."\">{$row->nick}</a>";
-						}
-						?>
-						<span>
-							<?php
-							echo pluralize($authors);
-							?>
-						</span>
-					</div>
-					<div class="row d-flex justify-content-between">
-						<span>Crew(s):</span>
-						<?php
-						$crews = [];
-						foreach (fetchAll("SELECT w.name as crew FROM collys c LEFT JOIN collys_crews cc ON cc.colly_id=c.id LEFT JOIN crews w ON w.id=cc.crew_id 
-							WHERE c.filename=:filename", [":filename" => $filename]) as $row) {
-							$crews[] = "<a href=\"/crew/".urlsafe($row->crew)."\">{$row->crew}</a>";
-						}
-						?>
-						<span clas="truncate">
-							<?php
-							echo pluralize($crews);
-							?>
-						</span>
-					</div>
-
-					<div class="row d-flex justify-content-between">
-						<span>Filename:</span>
-						<span><?=$colly->filename?></span>
-					</div>
-					<div class="row d-flex justify-content-between">
-						<span>Size:</span>
-						<span><?=formatBytes($colly->filesize)?></span>
-					</div>
-					<div class="row d-flex justify-content-between">
-						<span>Released:</span>
-						<span>
-							<?php
-							if (!empty($prodday)) {
-								echo "<span>{$prodday}</span>";
-							}
-							if (isset($prodmonth) && $month_list[ $prodmonth ] !== "Unknown") {
-								echo " {$month_list[$prodmonth]} ";
-							}
-							if (!empty($year)) {
-								echo $year;
-							}
-							?>
-						</span>
-					</div>
-
-					<div class="row d-flex justify-content-between">
-						<span>Rating:</span>
-
-						<?php
-						foreach (fetchAll("SELECT rating FROM collys WHERE filename = :filename", [":filename" => $filename]) as $row) {
-							$collyrating = $row->rating;
-						}
-
-						foreach (fetchAll("SELECT COUNT(rating) votes FROM comments WHERE filename = :filename", [":filename" => $filename]) as $row) {
-							$votecount = $row->votes;
-						}
-						?>
-						<?php
-						if ($votecount<3) {
-							$votesleft = (3 - $votecount);
-							echo "Awaiting $votesleft vote";
-							if ($votesleft > 1) {
-								echo "s";
-							}
-						} else {
-							echo "$collyrating ($votecount votes)";
-						}
-						?>
-					</div>
-					<div class="row d-flex justify-content-between">
-						<span>Added by:</span>
-						<span><a href="/member/<?=urlsafe($uploader)?>"><?=$uploader?></a></span>
-					</div>
-					<div class="row d-flex justify-content-between">
-						<span>Viewed:</span>
-						<span><?=$viewtimes?> times</span>
-					</div>
-					<div class="row d-flex justify-content-between">
-						<span>Downloaded:</span>
-						<?php
-						$downloads = fetchOne("SELECT downloads FROM collys WHERE filename = :filename", [":filename" => $filename])->downloads ?? 0;
-						?>
-						<span>
-							<?php
-							echo "{$downloads} Time";
-							if ($downloads !== 1) {
-								echo "s";
-							}
-							?>
-						</span>
-						<?php
+						$authors[] = "<a class=\"green\" href=\"/artist/".urlsafe($author)."\">{$row->nick}</a>";
 					}
 					?>
+					<span>
+						<?php
+						echo pluralize($authors);
+						?>
+					</span>
 				</div>
+				<div class="row d-flex justify-content-between">
+					<span>Crew(s):</span>
+					<?php
+					$crews = [];
+					foreach (fetchAll("SELECT w.name as crew FROM collys c LEFT JOIN collys_crews cc ON cc.colly_id=c.id LEFT JOIN crews w ON w.id=cc.crew_id 
+						WHERE c.filename=:filename", [":filename" => $filename]) as $row) {
+						$crews[] = "<a href=\"/crew/".urlsafe($row->crew)."\">{$row->crew}</a>";
+				}
+				?>
+				<span clas="truncate">
+					<?php
+					echo pluralize($crews);
+					?>
+				</span>
 			</div>
+
+			<div class="row d-flex justify-content-between">
+				<span>Filename:</span>
+				<span><?=$colly->filename?></span>
+			</div>
+			<div class="row d-flex justify-content-between">
+				<span>Size:</span>
+				<span><?=formatBytes($colly->filesize)?></span>
+			</div>
+			<div class="row d-flex justify-content-between">
+				<span>Released:</span>
+				<span>
+					<?php
+					if (!empty($prodday)) {
+						echo "<span>{$prodday}</span>";
+					}
+					if (isset($prodmonth) && $month_list[ $prodmonth ] !== "Unknown") {
+						echo " {$month_list[$prodmonth]} ";
+					}
+					if (!empty($year)) {
+						echo $year;
+					}
+					?>
+				</span>
+			</div>
+
+			<div class="row d-flex justify-content-between">
+				<span>Rating:</span>
+
+				<?php
+				foreach (fetchAll("SELECT rating FROM collys WHERE filename = :filename", [":filename" => $filename]) as $row) {
+					$collyrating = $row->rating;
+				}
+
+				foreach (fetchAll("SELECT COUNT(rating) votes FROM comments WHERE filename = :filename", [":filename" => $filename]) as $row) {
+					$votecount = $row->votes;
+				}
+				?>
+				<?php
+				if ($votecount<3) {
+					$votesleft = (3 - $votecount);
+					echo "Awaiting $votesleft vote";
+					if ($votesleft > 1) {
+						echo "s";
+					}
+				} else {
+					echo "$collyrating ($votecount votes)";
+				}
+				?>
+			</div>
+			<div class="row d-flex justify-content-between">
+				<span>Added by:</span>
+				<span><a href="/member/<?=urlsafe($uploader)?>"><?=$uploader?></a></span>
+			</div>
+			<?php
+			echo "type $type";
+			if (strpos($type, "ascii") !== false) {
+				?>
+				<div class="row d-flex justify-content-between">
+					<span>Viewed:</span>
+					<span><?=$viewtimes?> times</span>
+				</div>
+				<?php
+			}
+			echo "type $type";
+			if (strpos($type, "ansi") !== false) {
+				?>
+				<div class="row d-flex justify-content-between">
+					<span>Viewed:</span>
+					<span><?=$viewtimes?> times</span>
+				</div>
+				<?php
+			}
+			?>
+			<div class="row d-flex justify-content-between">
+				<span>Downloaded:</span>
+				<?php
+				$downloads = fetchOne("SELECT downloads FROM collys WHERE filename = :filename", [":filename" => $filename])->downloads ?? 0;
+				?>
+				<span>
+					<?php
+					echo "{$downloads} Time";
+					if ($downloads !== 1) {
+						echo "s";
+					}
+					?>
+				</span>
+				<?php
+			}
+			?>
 		</div>
 	</div>
-	<?php
-	?>
+</div>
+</div>
+<?php
+?>
