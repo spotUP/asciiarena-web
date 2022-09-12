02e4c51cb8033096ea5a9699eef0f09c776520ba
phantasm4489 <darren_m_coles@yahoo.co.uk>
2022-09-12 23:07:42 +0100

    add uhcsearch index page

---
+++ uhcsearch.php
@@ -0,0 +1,16 @@
+<?php  
+header('Content-Type: text/plain');  
+header("Content-Transfer-Encoding: utf-8");   
+require_once "session.php";
+
+foreach(fetchAll("SELECT * FROM collys") as $row) {
+  if (strlen($row->filename)>0) {
+  $dirname = explode(".", $row->filename);
+  $dirname = $dirname[0];
+  if (file_exists("collections/".$dirname."/".$row->filename)) {?>
+<?=$row->filename?><?=chr(9)?>https://asciiarena.se/collections/<?=$dirname?>/<?=$row->filename?><?=chr(9)?><?=$row->name?><?=chr(13).chr(10)?>
+<?php
+  }
+  }
+}
+?>
