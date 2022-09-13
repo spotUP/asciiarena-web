a281fb00224296bbe0c070270d64a5031c587dff
phantasm4489 <darren_m_coles@yahoo.co.uk>
2022-09-13 20:42:51 +0100

    create temporary uhcsearch2.php page to help with utf8 issues in db

--- uhcsearch2.php
+++ uhcsearch2.php
@@ -1,7 +1,7 @@
 <?php  
 header('Content-Type: text/plain; charset=ISO-8859-1');  
 require_once "session.php";
-echo "Filename".chr(9)."Path".chr(9)."Artist(s)".chr(9)."Crew(s)".chr(9)."Description".chr(10);
+echo "Filename".chr(9)."Path".chr(9)."Artist(s)".chr(9)."Crew(s)".chr(9)."Description".chr(9)."Description2".chr(10);
 
 foreach(fetchAll("select co.name, co.filename, (SELECT GROUP_CONCAT(c.name) FROM `collys_crews` cc inner join crews c on c.id = cc.crew_id where cc.colly_id=co.id group by cc.colly_id) crews, (SELECT GROUP_CONCAT(a.nick) FROM `artists_collys` ac inner join artists a on a.id= ac.artist_id where ac.colly_id=co.id group by ac.colly_id) artists from collys co") as $row) {
   if (strlen($row->filename)>0) {
