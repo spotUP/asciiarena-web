df2cbfbe9026d9297102f90ba01b4be8b1822b3d
Johan Samuelsson <johan.samuelsson@guardtools.com>
2022-09-16 22:23:12 +0200

    added type=archive option back

--- edit_colly.php
+++ edit_colly.php
@@ -269,6 +269,7 @@
 						<select class="select2" name="type" id="colly_type">
 							<option value="ASCII">ASCII</option>
 							<option value="ANSI">ANSI</option>
+							<option value="ANSI">Archive</option>
 						</select>
 					</div>
 				</div>
