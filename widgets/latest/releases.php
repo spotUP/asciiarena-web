4c035507331180ff8a1933c32a206d7c58b57f50
Johan Samuelsson <johan.samuelsson@guardtools.com>
2022-09-21 17:08:43 +0200

    nope

--- widgets/latest/releases.php
+++ widgets/latest/releases.php
@@ -8,56 +8,43 @@ if(empty($breakpoint)) {
 $breakpoint = rtrim($breakpoint, "-") . "-";
 $releases = [];
 $res = fetchAll("select * from (
-	(SELECT 'A' as type, filename, year(from_unixtime(timestamp)) fyear,month(from_unixtime(timestamp)) fmonth,dayofmonth(from_unixtime(timestamp)) fday from apps order by id desc limit 10)
-	union
-	(select 'C' as type, filename, year fyear, month fmonth, day fday from collys order by year desc, month desc,day desc limit 10)
-	union
-	(SELECT 'M' as type, filename, year(from_unixtime(timestamp)) fyear,month(from_unixtime(timestamp)) fmonth,dayofmonth(from_unixtime(timestamp)) fday from mags order by timestamp desc limit 10)
-	) a
-	order by a.fyear desc, a.fmonth desc, a.fday desc limit 10");
+(SELECT 'A' as type, filename, year(from_unixtime(timestamp)) fyear,month(from_unixtime(timestamp)) fmonth,dayofmonth(from_unixtime(timestamp)) fday from apps order by id desc limit 10)
+union
+(select 'C' as type, filename, year fyear, month fmonth, day fday from collys order by year desc, month desc,day desc limit 10)
+union
+(SELECT 'M' as type, filename, year(from_unixtime(timestamp)) fyear,month(from_unixtime(timestamp)) fmonth,dayofmonth(from_unixtime(timestamp)) fday from mags order by timestamp desc limit 10)
+) a
+order by a.fyear desc, a.fmonth desc, a.fday desc limit 10");
 foreach($res as $row) {
 	$dirname = explode(".", $row->filename);
 
-	if(strcmp($row->type,"C")==0) {
-		$file_id = BASEDIR . "/collections/{$dirname[0]}/{$row->filename}.diz";
-		$filename = "/release/".$row->filename;
-	} else if(strcmp($row->type,"M")==0) {
-		$file_id = BASEDIR . "/mags/{$dirname[0]}/{$row->filename}.diz";
-		$filename = "/magazine/".$row->filename;
-	} else if(strcmp($row->type,"A")==0) {
-		$file_id = preg_replace('/\\.[^.\\s]{3,4}$/', '', $row->filename).'.diz';
-		$file_id ='apps/'.$file_id;
-		$filename = "/application/".$row->filename;
-	} else {
-		$file_id ='';
-		$filename = '';
-	}
+  if(strcmp($row->type,"C")==0) {
+  	$file_id = BASEDIR . "/collections/{$dirname[0]}/{$row->filename}.diz";
+    $filename = "/release/".$row->filename;
+  } else if(strcmp($row->type,"M")==0) {
+  	$file_id = BASEDIR . "/mags/{$dirname[0]}/{$row->filename}.diz";
+    $filename = "/magazine/".$row->filename;
+  } else if(strcmp($row->type,"A")==0) {
+    $file_id = preg_replace('/\\.[^.\\s]{3,4}$/', '', $row->filename).'.diz';
+    $file_id ='apps/'.$file_id;
+    $filename = "/application/".$row->filename;
+  } else {
+    $file_id ='';
+    $filename = '';
+  }
 
 	if((count($releases) < $columns) && strlen($file_id)>0 && file_exists($file_id)) {
 		$releases[ $filename ] = utf8_encode(file_get_contents($file_id));
 	}
 }
-?>
-<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-	<div class="carousel-inner">
-		<?php
-		foreach($releases as $fname => $release) 
-		{
-			?>
-			<div class="carousel-item">
-				<pre><a href="<?=$fname?>" class="ascii magenta"><?=$release?></a></pre>
-			</div>
-
-			<?php 
-		}
-		?>
+foreach($releases as $fname => $release) 
+{
+	?>
+	<div class="col-12 d-flex justify-content-center col-<?=$breakpoint?><?=(round(12 / $columns))?> overflow-hidden apt-1 apb-1">
+		<div class="row animate__animated animate__backInUp">
+			<pre><a href="<?=$fname?>" class="ascii magenta"><?=$release?></a></pre>
+		</div>
 	</div>
-	<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-		<span class="sr-only">Previous</span>
-	</a>
-	<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-		<span class="carousel-control-next-icon" aria-hidden="true"></span>
-		<span class="sr-only">Next</span>
-	</a>
-</div>
+	<?php 
+}
+?>
