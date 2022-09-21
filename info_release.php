cdaa718a2b64ed84f61db7d138042c097466503e
Johan Samuelsson <johan.samuelsson@guardtools.com>
2022-09-21 16:18:06 +0200

    added share buttons to releases

--- info_release.php
+++ info_release.php
@@ -71,9 +71,8 @@ require_once "header.php";?>
 
         <button id="btnGroupDrop1" type="button" class="btn btn-big amb-1 btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Share </button>
         <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-          <a class="dropdown-item" href="mailto:?Subject=<?=$site_title?>&amp;Body=Check%20out%20this%20ass%20kicking%20of%20colly!%20 <a href='<?=$site_url?>'><?=$site_url?></a>">Mail</a>
+          <a class="dropdown-item" href="mailto:?Subject=<?=$site_title?>&amp;Body=Check%20out%20<?=$show_colly_name?>%20at%20aSCIIaRENA!%20<?=$site_url?>">Mail</a>
           <a class="dropdown-item" href="http://www.facebook.com/sharer.php?u=<?=$site_url?>" target="_blank">Facebook</a>
-          <a class="dropdown-item" href="javascript:void((function()%7Bvar%20e=document.createElement('script');e.setAttribute('type','text/javascript');e.setAttribute('charset','UTF-8');e.setAttribute('src','http://assets.pinterest.com/js/pinmarklet.js?r='+Math.random()*99999999);document.body.appendChild(e)%7D)());">Pinterest</a>
           <a class="dropdown-item" href="http://reddit.com/submit?url=<?=$site_url?>&amp;title=<?=$site_title?>" target="_blank">Reddit</a>
           <a class="dropdown-item" href="https://twitter.com/share?url=<?=$site_url?>&amp;text=<?=$twitterdesc?>" target="_blank">Twitter</a>
         </div>
