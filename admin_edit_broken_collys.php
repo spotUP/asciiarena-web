3e70546539cebe7364365b96eb68921ef66e99eb
Johan Samuelsson <johan.samuelsson@guardtools.com>
2022-09-22 09:45:13 +0200

    added edit button, but it doesn't work..

--- admin_edit_broken_collys.php
+++ admin_edit_broken_collys.php
@@ -29,20 +29,20 @@
 
 			<div class="row apl-1 apr-1 apb-1">
 				<div class="col-2 d-flex">
-					<input type="hidden" name="filename" value="<?=$filename?>">
-					<input type="submit" class="btn-big white bg-green" name="colly_fixed" value="Fixed">
-					<a href="/release/<?=$filename?>" class="btn-big amb-1 bg-header text apt-1 apb-1 grey-text" role="button" aria-disabled="true">View</a>
+					<?php if (is_logged_in()) { ?>
 
-
-      <form action="/admin.php#colly" method="post" id="edit-colly">
-        <input type="hidden" name="getcollyname" value="<?=$filename?>">
-        <input type="hidden" name="open_edit_colly_field" value="1">
-        <input type="submit" class="btn-big amb-1" name="edit_colly" value="Edit">
-      </form>
-
+						<input type="hidden" name="filename" value="<?=$filename?>">
+						<input type="submit" class="btn-big white bg-green" name="colly_fixed" value="Fixed">
+						<a href="/release/<?=$filename?>" class="btn-big amb-1 bg-header text apt-1 apb-1 grey-text" role="button" aria-disabled="true">View</a>
+						<form action="/admin.php#colly" method="post" id="edit-colly">
+							<input type="hidden" name="getcollyname" value="<?=$filename?>">
+							<input type="hidden" name="open_edit_colly_field" value="1">
+							<input type="submit" class="btn-big amb-1" name="edit_colly" value="Edit">
+						</form>
+					<?php } ?>
 				</div>
 			</div>
-			
+
 		</form>
 		<?php
 	}
