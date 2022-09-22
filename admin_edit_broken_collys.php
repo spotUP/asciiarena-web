098bace91261450c92b82327f63986ac6a4f99b9
Johan Samuelsson <johan.samuelsson@guardtools.com>
2022-09-22 09:30:33 +0200

    add view button to admin edit broken collys page

--- admin_edit_broken_collys.php
+++ admin_edit_broken_collys.php
@@ -5,39 +5,38 @@
 ?>
 <div class="tab-pane fade apt-1" id="broken">
 
-		<?php
-		$ask="SELECT * FROM collys WHERE broken='1' ORDER BY filename ASC";
-		$result=fetchAll($ask);
-		foreach ($result as $row)
-		{
-			$filename=$row->filename;
-			$broken_comment=$row->broken_comment;
+	<?php
+	$ask="SELECT * FROM collys WHERE broken='1' ORDER BY filename ASC";
+	$result=fetchAll($ask);
+	foreach ($result as $row)
+	{
+		$filename=$row->filename;
+		$broken_comment=$row->broken_comment;
 
-			?>
-	<form enctype="multipart/form-data" action="#broken" method="post">
-				<div class="row apb-1">
-					<div class="col-12">
-						<h1 class="ap-1 bg-header"><a href="/release/<?=$filename?>"><?=$filename?></a></h1>
-					</div>
-				</div>
-
-				<div class="row apl-1 apr-1 apb-1">
-					<div class="col-10">
-						<span class="cyan">Comment: <?=$broken_comment?></span>
-					</div>
-				</div>
-
-				<div class="row apl-1 apr-1 apb-1">
-					<div class="col-2">
-						<input type="hidden" name="filename" value="<?=$filename?>">
-						<input type="submit" class="btn-big white bg-green" name="colly_fixed" value="Fixed">
-					</div>
-				</div>
-				<div class="row" style="background-color: #111111;">
-					<br>
-				</div>
-	</form>
-			<?php
-		}
 		?>
+		<form enctype="multipart/form-data" action="#broken" method="post">
+			<div class="row apb-1">
+				<div class="col-12">
+					<h1 class="ap-1 bg-header"><a href="/release/<?=$filename?>"><?=$filename?></a></h1>
+				</div>
+			</div>
+
+			<div class="row apl-1 apr-1 apb-1">
+				<div class="col-10">
+					<span class="cyan">Comment: <?=$broken_comment?></span>
+				</div>
+			</div>
+
+			<div class="row apl-1 apr-1 apb-1">
+				<div class="col-2 d-flex">
+					<input type="hidden" name="filename" value="<?=$filename?>">
+					<input type="submit" class="btn-big white bg-green" name="colly_fixed" value="Fixed">
+					<a href="/release/<?=$filename?>" class="btn-big amb-1 bg-header text apt-1 apb-1 grey-text" role="button" aria-disabled="true">View</a>
+				</div>
+			</div>
+			
+		</form>
+		<?php
+	}
+	?>
 </div>
