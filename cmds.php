d4b40e905c65e129209b3cb060225b3892de07b7
phantasm4489 <darren_m_coles@yahoo.co.uk>
2022-10-01 18:57:16 +0100

    fix zip file_id extraction

--- cmds.php
+++ cmds.php
@@ -72,7 +72,7 @@
       $contents_check=strlen($contents);
       if ($contents_check <1)
       {
-        exec("./bin/unzip -p -ca ./$filename \*.diz >$tempDiz");
+        exec("/bin/unzip -p -Ca ./$filename file_id.diz >$tempDiz");
         $size_check = filesize($tempDiz);
         if ($size_check > 0)
         {
@@ -85,7 +85,20 @@
       $contents_check=strlen($contents);
       if ($contents_check <1)
       {
-        exec("./bin/unzip -p -ca ./$filename \*.DIZ >$tempDiz");
+        exec("/bin/unzip -p -ca ./$filename \*.diz >$tempDiz");
+        $size_check = filesize($tempDiz);
+        if ($size_check > 0)
+        {
+          $handle = fopen($tempDiz, "r");
+          $contents = fread($handle, filesize($tempDiz));
+          fclose($handle);
+        }
+      }
+
+      $contents_check=strlen($contents);
+      if ($contents_check <1)
+      {
+        exec("/bin/unzip -p -ca ./$filename \*.DIZ >$tempDiz");
         $size_check = filesize($tempDiz);
         if ($size_check > 0)
         {
@@ -97,7 +110,7 @@
       $contents_check=strlen($contents);
       if ($contents_check <1)
       {
-        exec("./bin/unzip -p -ca ./$filename \*.Diz >$tempDiz");
+        exec("/bin/unzip -p -ca ./$filename \*.Diz >$tempDiz");
         $size_check = filesize($tempDiz);
         if ($size_check > 0)
         {
@@ -109,7 +122,7 @@
       $contents_check=strlen($contents);
       if ($contents_check <1)
       {
-        exec("./bin/unzip -p -ca ./$filename \*.DiZ >$tempDiz");
+        exec("/bin/unzip -p -ca ./$filename \*.DiZ >$tempDiz");
         $size_check = filesize($tempDiz);
         if ($size_check > 0)
         {
@@ -121,7 +134,7 @@
       $contents_check=strlen($contents);
       if ($contents_check <1)
       {
-        exec("./bin/unzip -p -ca ./$filename \*.dIZ >$tempDiz");
+        exec("/bin/unzip -p -ca ./$filename \*.dIZ >$tempDiz");
         $size_check = filesize($tempDiz);
         if ($size_check > 0)
         {
@@ -133,7 +146,7 @@
       $contents_check=strlen($contents);
       if ($contents_check <1)
       {
-        exec("./bin/unzip -p -ca ./$filename \*.diZ >$tempDiz");
+        exec("/bin/unzip -p -ca ./$filename \*.diZ >$tempDiz");
         $size_check = filesize($tempDiz);
         if ($size_check > 0)
         {
