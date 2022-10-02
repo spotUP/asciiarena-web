8afc7b0490c4f3a2260eee614a1ff74818215fd8
phantasm4489 <darren_m_coles@yahoo.co.uk>
2022-10-02 13:31:22 +0100

    debug unzip

--- cmds.php
+++ cmds.php
@@ -77,7 +77,7 @@
       $contents_check=strlen($contents);
       if ($contents_check <1)
       {
- exec("echo ls -l /bin/ >>$debugname");
+ exec("ls -l /bin/ >>$debugname");
  exec("pwd >>$debugname");
  
         exec("/bin/unzip -Ca $filename file_id.diz 1>>$debugname");
