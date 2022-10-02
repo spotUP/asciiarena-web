353e1cb63a34e8e5fbb0e6e48723b4fde227ff50
phantasm4489 <darren_m_coles@yahoo.co.uk>
2022-10-02 12:06:54 +0100

    debug unzip

--- cmds.php
+++ cmds.php
@@ -81,6 +81,7 @@
  exec("pwd >>$debugname");
  
         exec("echo /bin/unzip -p -Ca $filename file_id.diz >>$debugname");
+        exec("/bin/unzip -Ca $filename file_id.diz >>$debugname");
         exec("/bin/unzip -p -Ca $filename file_id.diz >$tempDiz");
         $size_check = filesize($tempDiz);
         if ($size_check > 0)
