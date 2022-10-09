211235690ac2e32292779c322d56638cd17c5dc1
Johan Samuelsson <johan.samuelsson@guardtools.com>
2022-10-09 22:46:10 +0200

    test

--- info_release.php
+++ info_release.php
@@ -188,7 +188,7 @@ require_once "header.php";?>
   if ($type == "ASCII") 
   {
     ?>
-    <div class="row ml-0 mr-0 amb-1 p-0 xs-m-0 xs-m-0 xs-p-0 s-m-0 justify-content-center align-items-center" style="background-color: <?=$bgcolor?>;" id="colly-div"><pre id="colly" style="font-family: <?=$font;?>; color: <?=$fgcolor?>; white-space: pre;"><?php
+    <div class="row ml-0 mr-0 amb-1 p-0 xs-m-0 xs-m-0 xs-p-0 s-m-0 justify-content-center align-items-center" style="width: 640px; background-color: <?=$bgcolor?>;" id="colly-div"><pre id="colly" style="font-family: <?=$font;?>; color: <?=$fgcolor?>; white-space: pre;"><?php
     if (file_exists("collections/{$dirname}/{$filename}")) 
     {
       $content = encodeFileText("collections/{$dirname}/{$filename}");
