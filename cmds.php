64d39bc50ab9196a7be219a504ac548baa2865fc
phantasm4489 <darren_m_coles@yahoo.co.uk>
2022-10-24 14:12:29 +0100

    discord post for new app/mag/colly

--- cmds.php
+++ cmds.php
@@ -639,6 +639,8 @@
         }
       }
 
+      exec("curl -H \"Content-Type: application/json\" -d '{\"username\": \"ASCII ARENA Uploads\", \"content\": \"A new ascii collection has just been uploaded to asciiarena.com by ".$_user[ "nick" ]." named ".addslashes($name)."\"}' \"https://discord.com/api/webhooks/1034089537544126494/MdurdpqtOlFhb2W2-N3xuRV4tW9lqXVhIDzl3tDez3W74A4QVCv3t0T70FVolfuGirYG\"");
+
       doQuery("update users set uploaded=uploaded+:pumped where id=:userid", [ 'pumped' => $filesize, 'userid' => $_user['id'] ]);
       exit(json_out(["status" => true], $response));
     }
@@ -712,6 +714,8 @@
       if(!doQuery($q, $data)) {
         exit(json_out(["status" => true], 400));        
       }
+
+      exec("curl -H \"Content-Type: application/json\" -d '{\"username\": \"ASCII ARENA Uploads\", \"content\": \"A new application has just been uploaded to asciiarena.com by ".$_user[ "nick" ]." named ".addslashes($name)."\"}' \"https://discord.com/api/webhooks/1034089537544126494/MdurdpqtOlFhb2W2-N3xuRV4tW9lqXVhIDzl3tDez3W74A4QVCv3t0T70FVolfuGirYG\"");
 
       doQuery("update users set uploaded=uploaded+:pumped where id=:userid", [ 'pumped' => $filesize, 'userid' => $_user['id'] ]);
       exit(json_out(["status" => true], $response));
@@ -789,6 +793,8 @@
       if(!doQuery($q, $data)) {
         exit(json_out(["status" => true], 400));        
       }
+
+      exec("curl -H \"Content-Type: application/json\" -d '{\"username\": \"ASCII ARENA Uploads\", \"content\": \"A new magazine has just been uploaded to asciiarena.com by ".$_user[ "nick" ]." named ".addslashes($name)."\"}' \"https://discord.com/api/webhooks/1034089537544126494/MdurdpqtOlFhb2W2-N3xuRV4tW9lqXVhIDzl3tDez3W74A4QVCv3t0T70FVolfuGirYG\"");     
       
       doQuery("update users set uploaded=uploaded+:pumped where id=:userid", [ 'pumped' => $filesize, 'userid' => $_user['id'] ]);
       exit(json_out(["status" => true], $response));
