409e763484d35b4b1e126a604c68de02abfdd6de
phantasm4489 <darren_m_coles@yahoo.co.uk>
2022-12-08 11:03:13 +0000

    force sort order for playlists

--- uhcplaylists.php
+++ uhcplaylists.php
@@ -25,7 +25,7 @@ if (!empty($file)) {
   header('Content-Type: text/plain; charset=ISO-8859-1');  
   echo "Filename".chr(9)."Path".chr(9)."Title".chr(9)."Author".chr(9)."Genre".chr(10);
 
-  foreach(fetchAll("select * from hippo_playlists") as $row) {
+  foreach(fetchAll("select * from hippo_playlists order by id") as $row) {
       echo utf8_decode($row->filename).chr(9)."playlist".chr(9).utf8_decode($row->title).chr(9).utf8_decode($row->author).chr(9).utf8_decode($row->genre).chr(10);
     }
 }
