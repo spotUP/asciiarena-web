2b485ebe2710c3290c35723c3a32f5c6ace5df03
phantasm4489 <darren_m_coles@yahoo.co.uk>
2023-11-10 17:52:06 +0000

    commit latest files from site

--- uhcsearch.php
+++ uhcsearch.php
@@ -16,7 +16,7 @@ foreach(fetchAll($q) as $row) {
   $dirname = explode(".", $row->filename);
   $dirname = $dirname[0];
   if (file_exists("collections/".$dirname."/".$row->filename)) {
-    echo utf8_decode($row->filename).chr(9)."collections/".utf8_decode($dirname).chr(9).utf8_decode($row->artists).chr(9).utf8_decode($row->crews).chr(9).utf8_decode($row->name).chr(10);
+    echo utf8_decode($row->filename ?? '').chr(9)."collections/".utf8_decode($dirname ?? '').chr(9).utf8_decode($row->artists ?? '').chr(9).utf8_decode($row->crews ?? '').chr(9).utf8_decode($row->name ?? '').chr(10);
   }
   }
 }
