6f7b5dff1d71dee1923bc57629a93c29819c956a
System Administrator <spotUP>
2023-11-15 00:54:37 +0100

    adjusted column size and text

--- requests.php
+++ requests.php
@@ -99,7 +99,7 @@ $sort_by = $_GET['sort_by'] ?? "id";
    <div id = "hdrcols" class="row amb-1">
     <div class="col-5"><span class="white"><a onclick="updateSort('title')">Title</a></span></div>
     <div class="col-2"><span class="white"><a onclick="updateSort('title')">Status</a></span></div>
-    <div class="col-2"><span class="white"><a onclick="updateSort('user')">Reqed By</a></span></div>
+    <div class="col-2"><span class="white"><a onclick="updateSort('user')">Reqed by</a></span></div>
     <div class="col-3"><span class="white"><a onclick="updateSort('user')">Date</a></span></div>
   </div>
 
