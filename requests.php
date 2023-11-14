083fa82a25dc241d3edcf306ec9fe1c481fb9029
System Administrator <spotUP>
2023-11-15 00:50:00 +0100

    adjusted column size

--- requests.php
+++ requests.php
@@ -97,9 +97,9 @@ $sort_by = $_GET['sort_by'] ?? "id";
    </div>
 
    <div id = "hdrcols" class="row amb-1">
-    <div class="col-3"><span class="white"><a onclick="updateSort('title')">Title</a></span></div>
-    <div class="col-3"><span class="white"><a onclick="updateSort('title')">Status</a></span></div>
-    <div class="col-3"><span class="white"><a onclick="updateSort('user')">Requested By</a></span></div>
+    <div class="col-5"><span class="white"><a onclick="updateSort('title')">Title</a></span></div>
+    <div class="col-2"><span class="white"><a onclick="updateSort('title')">Status</a></span></div>
+    <div class="col-2"><span class="white"><a onclick="updateSort('user')">Requested By</a></span></div>
     <div class="col-3"><span class="white"><a onclick="updateSort('user')">Date</a></span></div>
   </div>
 
@@ -221,13 +221,13 @@ function getRequests(page,sort,order,filter) {
       break
     }
     reqtxt = `<div class="row">
-    <div class="col-3 text-truncate">
+    <div class="col-6 text-truncate">
     <a class="magenta" href="${req.url}">${req.title}</a>
     </div>
-    <div class="col-3 text-truncate">
+    <div class="col-2 text-truncate">
     <a class="magenta" href="${req.url}">${status}</a>
     </div>
-    <div class="col-3 text-truncate">
+    <div class="col-2 text-truncate">
     <a class="magenta" href="${req.url}">${req.user}</a>
     </div>
     <div class="col-3 text-truncate">
