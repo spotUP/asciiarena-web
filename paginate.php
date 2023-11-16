36ceed4d33892a84034accee6c9757c71bea33e8
System Administrator <spotUP>
2023-11-16 01:50:18 +0100

    pagination fix

--- paginate.php
+++ paginate.php
@@ -7,7 +7,7 @@
       <li class="page-item"> <button class="btn btn-primary" style="width:50px;" onclick="firstPage(event)" href="#" class="page-link" ><<</button></li>
       <li class="page-item"> <button class="btn btn-primary" style="width:50px;" onclick="prevPage(event)" href="#" class="page-link" ><</button></li><span id="currpage"></span>
 
-      <button class="btn paginator w-100"><span id="currpage"></span></button>
+      <span id="currpage" class="paginator w-100"></span>
 
       <li class="page-item"> <button class="btn btn-primary" style="width:50px;" onclick="nextPage(event)" href="#" class="page-link" >></button></li>
       <li class="page-item"> <button class="btn btn-primary" style="width:50px;" onclick="lastPage(event)" href="#" class="page-link" >>></button></li>
