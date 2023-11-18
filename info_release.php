e7fbdb5b02960a964736e3dfbf2ed4a28b3ccef7
Johan Samuelsson <spotUP>
2023-11-18 13:43:44 +0100

    add overflow-y scroll

--- info_release.php
+++ info_release.php
@@ -177,7 +177,7 @@ require_once "header.php";?>
     if ($type == "ASCII") 
     {
       ?>
-      <div class="row ml-0 mr-0 amb-1 p-0 xs-m-0 xs-m-0 xs-p-0 s-m-0 justify-content-center align-items-center" style="background-color: <?=$bgcolor?>;">
+      <div class="row ml-0 mr-0 amb-1 p-0 xs-m-0 xs-m-0 xs-p-0 s-m-0 justify-content-center align-items-center" style="overflow-y: scroll; height: 100vh; background-color: <?=$bgcolor?>;">
         <div class="row ml-0 mr-0 amb-1 p-0 xs-m-0 xs-m-0 xs-p-0 s-m-0 justify-content-center align-items-center" style="background-color: <?=$bgcolor?>; width: 640px;" id="colly-div"><pre id="colly" style="font-family: <?=$font;?>; color: <?=$fgcolor?>; white-space: pre;"><?php
         if (file_exists("collections/{$dirname}/{$filename}")) 
         {
